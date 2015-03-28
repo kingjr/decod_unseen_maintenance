b6797145228aa8b119859f440e9e531f037d4de1
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-03-28 18:46:21 +0000

    corrected a bug in the SVR recombine function and plotting orientationSVR script

--- python/plot_decode_orientationSVR.py
+++ python/plot_decode_orientationSVR.py
@@ -16,40 +16,21 @@ from config import (
         data_path,
         inputTypes)
 from utils import get_data
+from myfunctions import (
+                    cart2pol,
+                    recombine_svr_prediction
+)
 
-
-################################################################################
-def recombine_svr_prediction(path_x,path_y):
-
-    #load first regressor (cosine)
-    with open(path_x) as f:
-        gat, contrast = pickle.load(f)
-    x = np.array(gat.y_pred_)
-    true_x = gat.y_train_
-
-    #load second regressor (sine)
-    with open(path_y) as f:
-        gat, contrast = pickle.load(f)
-    y = np.array(gat.y_pred_)
-
-    # cartesian 2 polar (radians) transformation
-    radius, angle = cart2pol(x, y)
-
-    return (angle, radius, true_x)
-
-
-def cart2pol(x, y):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    return(rho, phi)
-
-################################################################################
-
+# Define subjects and contrasts of interest
 #subjects = [subjects[i] for i in range(20) # XXX to be be removed
 contrasts = ['orientation_target']
-for typ in inputTypes:                                                      # Input type defines whether we decode ERFs or frequency power
+
+# Loop across conditions
+# Input type defines whether we decode ERFs or frequency power
+for typ in inputTypes:
     print(typ)
-    for freq in typ['values']:                                              # loop only once if ERF and across all frequencies of interest if frequency power
+    # loop only once if ERF and across all frequencies of interest if frequency
+    for freq in typ['values']:
         print(freq)
         for contrast in contrasts:
             # define condition string
@@ -78,40 +59,50 @@ for typ in inputTypes:                                                      # In
                     fname_appendix = op.join('_Tfoi_mtm_',freq,'Hz')
 
                 # load behavioral data
-                meg_fname = op.join(data_path, subject, 'preprocessed', subject + '_preprocessed' + fname_appendix)
-                bhv_fname = op.join(data_path, subject, 'behavior', subject + '_fixed.mat')
+                meg_fname = op.join(data_path, subject, 'preprocessed',
+                                    subject + '_preprocessed' + fname_appendix)
+                bhv_fname = op.join(data_path, subject, 'behavior',
+                                    subject + '_fixed.mat')
                 epochs, events = get_data(meg_fname, bhv_fname)
 
                 # Define path to retrieve cosine classifier
                 path_x = op.join(data_path, subject, 'mvpas',
-                    '{}-decod_{}_{}{}.pickle'.format(subject, cond_name_cos, clf_type,fname_appendix))
+                    '{}-decod_{}_{}{}.pickle'.format(subject, cond_name_cos,
+                                                    clf_type,fname_appendix))
 
                 # Define path to retrieve sine classifier
                 path_y = op.join(data_path, subject, 'mvpas',
-                    '{}-decod_{}_{}{}.pickle'.format(subject, cond_name_sin, clf_type,fname_appendix))
+                    '{}-decod_{}_{}{}.pickle'.format(subject, cond_name_sin,
+                                                    clf_type,fname_appendix))
 
-                # recombine cos and sin predictions to obtain the predicted angle in radians
-                predAngle, radius, trueX = recombine_svr_prediction(path_x,path_y)
+                # recombine cos and sin predictions to obtain
+                # the predicted angle in radians
+                predAngle, radius, trueX = recombine_svr_prediction(path_x,
+                                                                    path_y)
 
                 # retrieve angle presented in radians
-                trueAngle = np.rad2deg(np.arccos(trueX))
+                trueAngle = np.arccos(trueX)
 
                 # Compute several measures to estimate goodness of fit
                 ######################MEAN SQUARED ERROR########################
                 dims = predAngle.shape
-                error = ((predAngle.squeeze() - np.tile(trueAngle,np.append(dims[0:2],1)) % (2*np.pi)) - np.pi ) ** 2
+                error = ((predAngle.squeeze() - np.tile(trueAngle,
+                            np.append(dims[0:2],1)) % (2*np.pi)) - np.pi ) ** 2
 
                 # compute truth-prediction square error
-                error_grand[s,:,:] = error.mean(2)
+                error_grand[s, :, :] = error.mean(2)
 
                 ################ RAYLEIGH TEST CIRCULR  ########################
-                p_rayleigh[s,:,:], z_rayleigh[s,:,:] = rayleigh(2*predAngle.squeeze()-np.pi,axis=2)
+                p_rayleigh[s, :, :], z_rayleigh[s, :, :] = rayleigh(2 *
+                                            predAngle.squeeze() - np.pi, axis=2)
 
                 ###############  OMNIBUS TEST  #################################
-                p_omni[s,:,:], m_omni[s,:,:] = omnibus(2*predAngle.squeeze()-np.pi,axis=2)
+                p_omni[s, :, :], m_omni[s, :, :] = omnibus(2 *
+                                            predAngle.squeeze() - np.pi, axis=2)
 
                 ################## V-TEST ######################################
-                p_vtest[s,:,:], v_vtest[s,:,:] = vtest(2*predAngle.squeeze()-np.pi,0,axis=2)
+                p_vtest[s, :, :], v_vtest[s, :, :] = vtest(2 *
+                                         predAngle.squeeze() - np.pi, 0, axis=2)
     break
 
 plt.subplot(2, 2, 1)
