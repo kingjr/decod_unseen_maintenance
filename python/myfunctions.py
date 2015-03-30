7ca7573a189a8cd7887b801bac16c07bb68de349
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-03-30 19:27:33 +0100

    added a function for the plotting of a polar distribution around the circle

--- python/myfunctions.py
+++ python/myfunctions.py
@@ -59,3 +59,41 @@ def realign_angle(gat, dims, angles = [15, 45, 75, 105, 135, 165] ):
         order = np.array(range(a,n_classes)+range(0,(a)))
         probas[:, :, sel, :] = prediction[:, :, sel, :][:, :, :, order]
     return probas
+
+def plot_circ_hist(alpha, bins=10, measure='radians'):
+    """
+    This function plots a polar histogram of the distribution of circular data.
+    Input:
+    alpha: 1D distribution of circular data in radians
+    bins=number of bins the distribution has to be broken in. Default = 10
+    measure= plot labels: radians (default) | degrees
+    Output:
+    none
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+
+    bottom = 8
+    max_height = 4
+
+    hist, bin_edges = np.histogram(np.array(alpha),bins=80)
+    N = len(hist)
+    radii = np.ones(bin_edges.shape)
+    width = (2*np.pi) / N
+
+    ax = plt.subplot(111, polar=True)
+    bars = ax.bar(bin_edges[0:N], hist, width=width, bottom=bottom)
+
+    if measure == 'radians':
+        xT=plt.xticks()[0]
+        xL=['0',r'$\frac{\pi}{4}$',r'$\frac{\pi}{2}$',r'$\frac{3\pi}{4}$',\
+           r'$\pi$',r'$\frac{5\pi}{4}$',r'$\frac{3\pi}{2}$',r'$\frac{7\pi}{4}$']
+        plt.xticks(xT, xL)
+
+    # Use custom colors and opacity
+    # for r, bar in zip(radii, bars):
+    #     bar.set_facecolor(plt.cm.jet(r / 10.))
+    #     bar.set_alpha(0.8)
+
+    plt.show()
