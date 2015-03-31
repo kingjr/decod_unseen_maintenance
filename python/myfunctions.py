69477192143d59996137c8cf2ba1bff5d6af5aea
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-03-31 20:06:47 +0100

    added functions for computation of n-d histograms and svr orientation tuning curve

--- python/myfunctions.py
+++ python/myfunctions.py
@@ -3,22 +3,27 @@ results.
 Niccolo Pescetelli niccolo.pescetelli@psy.ox.ac.uk
 """
 
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def recombine_svr_prediction(path_x,path_y):
+def recombine_svr_prediction(path_x,path_y, res=10):
     """
     This function takes the paths of two classifiers SVR predictions, typically
     sine and cosine of an angle, and combine them into a predicted angle in
     radians
     """
-    import pickle
-    import numpy as np
+
+    # define angles in degrees
+    angles = np.linspace(15, 165, 6)
 
     #load first regressor (cosine)
     with open(path_x) as f:
         gat, contrast = pickle.load(f)
     x = np.array(gat.y_pred_)
-    true_x = gat.y_train_
+    # retrieve true predictor (eg. true cosine)
+    trueX = gat.y_train_
 
     #load second regressor (sine)
     with open(path_y) as f:
@@ -26,15 +31,76 @@ def recombine_svr_prediction(path_x,path_y):
     y = np.array(gat.y_pred_)
 
     # cartesian 2 polar (radians) transformation
-    angle, radius = cart2pol(x, y)
+    theta, radius = cart2pol(x, y)
 
-    # take only values within 2 pi radians
-    angle = (angle % (2 * np.pi)) - np.pi
-    return (angle, radius, true_x)
+    # take only values within 0 to 2 pi radians range
+    theta = (theta % (2 * np.pi))
 
+    # define dimensions of theta
+    dims = theta.shape
+
+    # realign to get single tuning curve across angles
+    predict_angle = np.zeros(dims);
+    for a in angles:
+        # true angle in radians
+        true_rad = np.arccos(trueX)
+        # true angle in degrees
+        true_deg = np.rad2deg(true_rad)
+        # round values
+        true_angle = np.round(true_deg)
+
+        # select trials with angles a
+        sel = true_angle == a
+        # relign across angle categories
+        predict_angle[:,:,sel,0] = ((np.pi + theta[:,:,sel,0] - 2*np.deg2rad(a))
+                                    / 2) % np.pi
+
+    # compute proportion of trials correctly predicted
+    N = histogramnd(predict_angle.squeeze() - np.pi/2,
+                                    bins=borns(-np.pi,np.pi/2,res+1),
+                                    axis = 2)
+    # extract frequencies
+    trial_freq = N[0]
+
+    # Wrap around first and last bin
+    trial_freq[1,:,:] = trial_freq[1,:,:]+trial_freq[-1,:,:]
+    trial_freq[-1,:,:] = trial_freq[1,:,:]
+
+    #compute totals
+    totals = np.tile(np.sum(trial_freq,axis=2),[res,1,1])
+    # compute the proportion of trials in each bin
+    trial_prop = trial_freq / totals.astype(np.float).transpose([1, 2, 0])
+
+    return (theta, radius, trueX, trial_prop)
+
+# The way it is done in Matlab XXX to be removed
+#
+# % realign to get single tuning curve across angles
+# predict_angle = [];
+# for a = 6:-1:1
+#     % select trials with angles a
+#     sel = angles==a;
+#     % relign across angle categories
+#     predict_angle(sel,:,:) = mod((pi+mod(theta(sel,:,:),2*pi)-2*deg2rad(-15+30*a))/2,pi);
+# end
+# sel = isnan(angles);
+# predict_angle(sel,:,:) = NaN;
+#
+#
+# % compute proportion of trials correctly predicted
+# trial_proportion = hist(predict_angle-pi/2,borns(-pi/2,pi/2,res));
+# trial_proportion = reshape(trial_proportion,[size(trial_proportion,1) size(predict_angle,2) size(predict_angle,3)]);
+# trial_proportion(1,:,:) = trial_proportion(1,:,:)+trial_proportion(end,:,:);
+# trial_proportion(end,:,:) = trial_proportion(1,:,:);
+# % get proportion of trials
+# trial_proportion = trial_proportion./repmat(sum(trial_proportion),[size(trial_proportion,1),1,1]);
+
+
+def borns(m, M, n):
+    out = np.arange(m+(M-m)/n/2,(M+(M-m)/n/2),(M-m)/n)
+    return out
 
 def cart2pol(x, y):
-    import numpy as np
     radius = np.sqrt(x**2 + y**2)
     theta = np.arctan2(y, x)
     return(theta,radius)
@@ -49,7 +115,6 @@ def realign_angle(gat, dims, angles = [15, 45, 75, 105, 135, 165] ):
     Output:
     probas : a time x time x trials x class array
     """
-    import numpy as np
     # realign to 4th angle category
     probas = np.zeros(dims)
     n_classes = len(angles)
@@ -70,9 +135,6 @@ def plot_circ_hist(alpha, bins=10, measure='radians'):
     Output:
     none
     """
-    import numpy as np
-    import matplotlib.pyplot as plt
-
 
     bottom = 8
     max_height = 4
@@ -97,3 +159,145 @@ def plot_circ_hist(alpha, bins=10, measure='radians'):
     #     bar.set_alpha(0.8)
 
     plt.show()
+
+
+# def hist3d(sample, bins=10, range=None, normed=False, weights=None):
+#     r=np.random.randn(100,3,3)
+#     r_ = r.reshape((r.shape[0], np.prod(r.shape[1:])))
+#     H, edges = histogramnd(r_,tile(bins))
+#     H.reshape((H.shape[0], r.shape[1:])
+
+
+# License: Scipy compatible
+# Author: David Huard, 2006
+# http://projects.scipy.org/numpy/attachment/ticket/189/histogram1d.py
+def histogramnd(a, bins=10, range=None, normed=False, weights=None, axis=None):
+    """histogram(a, bins=10, range=None, normed=False, weights=None, axis=None)
+                                                                   -> H, dict
+
+    Return the distribution of sample.
+
+    Parameters
+    ----------
+    a:       Array sample.
+    bins:    Number of bins, or
+             an array of bin edges, in which case the range is not used.
+    range:   Lower and upper bin edges, default: [min, max].
+    normed:  Boolean, if False, return the number of samples in each bin,
+             if True, return a frequency distribution.
+    weights: Sample weights. The weights are normed only if normed is True.
+             Should weights.sum() not equal len(a), the total bin count will
+             not be equal to the number of samples.
+    axis:    Specifies the dimension along which the histogram is computed.
+             Defaults to None, which aggregates the entire sample array.
+
+    Output
+    ------
+    H:            The number of samples in each bin.
+                  If normed is True, H is a frequency distribution.
+    dict{
+    'edges':      The bin edges, including the rightmost edge.
+    'upper':      Upper outliers.
+    'lower':      Lower outliers.
+    'bincenters': Center of bins.
+    }
+
+    Examples
+    --------
+    x = random.rand(100,10)
+    H, Dict = histogram(x, bins=10, range=[0,1], normed=True)
+    H2, Dict = histogram(x, bins=10, range=[0,1], normed=True, axis=0)
+
+    See also: histogramnd
+    """
+
+    a = np.asarray(a)
+    if axis is None:
+        a = atleast_1d(a.ravel())
+        axis = 0
+
+    # Bin edges.
+    if not np.iterable(bins):
+        if range is None:
+            range = (a.min(), a.max())
+        mn, mx = [mi+0.0 for mi in range]
+        if mn == mx:
+            mn -= 0.5
+            mx += 0.5
+        edges = np.linspace(mn, mx, bins+1, endpoint=True)
+    else:
+        edges = np.asarray(bins, float)
+
+    dedges = np.diff(edges)
+    decimal = int(-np.log10(dedges.min())+6)
+    bincenters = edges[:-1] + dedges/2.
+
+    # apply_along_axis accepts only one array input, but we need to pass the
+    # weights along with the sample. The strategy here is to concatenate the
+    # weights array along axis, so the passed array contains [sample, weights].
+    # The array is then split back in  __hist1d.
+    if weights is not None:
+        aw = concatenate((a, weights), axis)
+        weighted = True
+    else:
+        aw = a
+        weighted = False
+
+    count = np.apply_along_axis(__hist1d, axis, aw, edges, decimal, weighted, normed)
+
+    # Outlier count
+    upper = count.take(np.array([-1]), axis)
+    lower = count.take(np.array([0]), axis)
+
+    # Non-outlier count
+    core = a.ndim*[slice(None)]
+    core[axis] = slice(1, -1)
+    hist = count[core]
+
+    if normed:
+        normalize = lambda x: atleast_1d(x/(x*dedges).sum())
+        hist = np.apply_along_axis(normalize, axis, hist)
+
+    return hist, {'edges':edges, 'lower':lower, 'upper':upper, \
+        'bincenters':bincenters}
+
+
+# License: Scipy compatible
+# Author: David Huard, 2006
+# http://projects.scipy.org/numpy/attachment/ticket/189/histogram1d.py
+def __hist1d(aw, edges, decimal, weighted, normed):
+    """Internal routine to compute the 1d histogram.
+    aw: sample, [weights]
+    edges: bin edges
+    decimal: approximation to put values lying on the rightmost edge in the last
+             bin.
+    weighted: Means that the weights are appended to array a.
+    Return the bin count or frequency if normed.
+    """
+    nbin = edges.shape[0]+1
+    if weighted:
+        count = zeros(nbin, dtype=float)
+        a,w = hsplit(aw,2)
+        if normed:
+            w = w/w.mean()
+    else:
+        a = aw
+        count = np.zeros(nbin, dtype=int)
+        w = None
+
+    binindex = np.digitize(a, edges)
+
+    # Values that fall on an edge are put in the right bin.
+    # For the rightmost bin, we want values equal to the right
+    # edge to be counted in the last bin, and not as an outlier.
+    on_edge = np.where(np.around(a,decimal) == np.around(edges[-1], decimal))[0]
+    binindex[on_edge] -= 1
+
+    # Count the number of identical indices.
+    flatcount = np.bincount(binindex, w)
+
+    # Place the count in the histogram array.
+    i = np.arange(len(flatcount))
+    count[i] = flatcount
+
+    return count
