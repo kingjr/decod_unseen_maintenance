b410a5bba1ed0f1cd752ca1c4cdf26f526437a41
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-04-02 14:58:56 +0100

    modification to account for true across subjects stats computation. not sure though

--- python/temp/decod_align_vtest.py
+++ python/temp/decod_align_vtest.py
@@ -1,84 +1,89 @@
+"""
+This script implements two different versions of how to compute across
+subjects stats.
+A) It computes across subjects stats by computing stats
+within each subject (across trials) and then averaging their results. This
+method lacks power.
+B) A real and more powerful across subjects implementation  is obtained by
+computing the distributions in each subject and then compute the test
+across subjects.
+
+niccolo.pescetelli@psy.ox.ac.uk
+"""
 import pickle
 import os.path as op
 import numpy as np
+
 import matplotlib.pyplot as plt
 from pycircstat import vtest, rayleigh
-from config import (subjects, data_path)
-from myfunctions import realign_angle, cart2pol, plot_circ_hist
 
-# tmp
-#subjects=[subjects[i] for i in range(10)]
+from config import (subjects, results_path)
+from postproc_functions import realign_angle, cart2pol, plot_circ_hist
 
 # This is the stats across trials, but really we only need to apply a similar
-# thing across subjects.
-p_val = list()
-angle_error_across_weighted  = np.zeros([29, 29, 400, 20])
-angle_error_across_argmax = np.zeros([29, 29, 400, 20])
+# thing across subjects. This is done in orientation_stats_across.py
 for s, subject in enumerate(subjects):
     print(subject)
-    path_gat = op.join(data_path, subject, 'mvpas',
-        '{}-decod_{}_{}.pickle'.format(subject, 'orientation_target', 'SVC'))
+
+    # define results path
+    path_gat = op.join(results_path, subject, 'mvpas',
+        '{}-decod_{}_{}.pickle'.format(subject, 'targetAngle', 'SVC'))
 
     ###### PREPROC
     # Compute angle errors by realigning the predicted categories according to
     # true category
+
     # read GAT data
     with open(path_gat) as f:
-        gat, contrast = pickle.load(f)
+        gat, contrast, _, events = pickle.load(f)
 
     n_time, n_test_time, n_trials, n_categories = dims = np.shape(gat.y_pred_)
+    # initialize variables if first subject
+    if s == 0:
+        p_val = list()
+        M=list()
+        dims_=np.array(dims)
+        angle_error_across_w = np.zeros(np.append(dims_[[0,1,2]],len(subjects)))
+
     # realign to 4th angle category
-    probas = realign_angle(gat, dims)
+    probas = realign_angle(gat)
 
     # transform 6 categories into single angle: there are two options here,
     # try it on the pilot subject, and use weighted mean if the two are
     # equivalent
-    prob2angle = 'weighted_mean'
-    if prob2angle == 'most_likely_angle':
-        angle_errors = np.argmax(probas, axis=3) * np.pi / 3 - np.pi / 6
-    elif prob2angle == 'weighted_mean':
-        operator = np.tile(np.arange(n_categories),
-                         (n_time, n_time, n_trials, 1))
-        weighted_errors = (probas * operator)* np.pi / 3 - np.pi / 6
-        x = np.mean(np.cos(weighted_errors),  axis=3)
-        y = np.mean(np.sin(weighted_errors), axis=3)
-        angle_errors_weighted, radius = cart2pol(x,y)
-
-    # compare weighted mean with argmax selection. To be removed once decided XXX
-    angle_errors_argmax = np.argmax(probas, axis=3) * np.pi / 3 - np.pi / 6
+    operator = np.tile(np.arange(n_categories),
+                     (n_time, n_time, n_trials, 1))
+    weighted_errors = (probas * operator)* np.pi / 3 - np.pi / 6
+    x = np.mean(np.cos(weighted_errors),  axis=3)
+    y = np.mean(np.sin(weighted_errors), axis=3)
+    angle_errors_w, radius = cart2pol(x,y)
 
     ####### STATS WITHIN SUBJECTS
     # Apply v test and retrieve statistics that is independent of the number of
     # trials.
-    p_val_v, _ = vtest(angle_errors_weighted, 0, axis=2) # trials x time
+    p_val_v, V = vtest(angle_errors_w, 0, axis=2) # trials x time
 
     # apply Rayleigh test (better for of diagonal with distribution shifts, e.g.
     # on the n200, the prediction may reverse)
-    p_val_ray, _ = rayleigh(angle_errors_weighted, axis=2) # trials x time
+    p_val_ray, Z = rayleigh(angle_errors_w, axis=2) # trials x time
 
     # append scores
+    M.append([V,Z])
     p_val.append([p_val_v, p_val_ray])
 
     ####### STATS ACROSS SUBJECTS
     # apply Rayleigh test (better for of diagonal with distribution shifts, e.g.
     # on the n200, the prediction may reverse)
-    angle_error_across_weighted[:, :, :, s] = angle_errors_weighted
-    angle_error_across_argmax[:, :, :, s] = angle_errors_argmax
-
+    # angle_error_across_w[:, :, :, s] = angle_errors_w
 
 # plot error, ie class distance
 plt.subplot(221)
-plt.imshow(-mean(mean(angle_error_across_weighted,axis=2),axis=2),interpolation='none',origin='lower')
+plt.imshow(-mean(mean(angle_error_across_w,axis=2),axis=2),interpolation='none',origin='lower')
 plt.colorbar()
 plt.title('weighted negative error')
 
-plt.subplot(222)
-plt.imshow(-mean(mean(angle_error_across_argmax,axis=2),axis=2),interpolation='none',origin='lower')
-plt.colorbar()
-plt.title('argmax negative error')
-
 # plot average Rayleigh p value
-plt.subplot(223)
+plt.subplot(222)
 mean_p_vals = np.zeros((n_time, n_test_time, len(subjects)))
 for s, subject in enumerate(subjects):
     mean_p_vals[:, :, s] = np.array(p_val[s][0])
@@ -87,8 +92,8 @@ plt.colorbar()
 plt.title('mean p-val Rayleigh')
 
 ## STATS ACROSS SUBJECTS - tentative, dubious method XXX
-plt.subplot(224)
-p, z = rayleigh(angle_error_across_weighted, axis=2, d=20)
+plt.subplot(223)
+p, z = rayleigh(angle_error_across_w, axis=2, d=20)
 plt.imshow(np.mean(z, axis=2), origin='lower',interpolation='none')
 plt.colorbar()
 plt.title('z Rayleigh (dubious method)')
