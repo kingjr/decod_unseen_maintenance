46ce3aef2d4bce950d0710f3577f1b2cff9dcde5
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-04-02 17:17:07 +0100

    plots tunining curves (on diagonal) for SVC and SVR classifiers

--- python/plot_orientation_tuning_curve.py
+++ python/plot_orientation_tuning_curve.py
@@ -31,19 +31,20 @@ from postproc_functions import (
 # input type is ERF (for now)
 inputType=inputTypes[0]
 # classifier type is SVR
-clf_type = clf_types[0]
+clf_type = clf_types[1]
 # contrast is target orientation sine and cosine (for now...)
 contrast = clf_type['contrasts'][0:2]
-
-# initialize variables
-res = 20
-trial_proportion = np.zeros([len(subjects),29,29,res])
 
 # loop across subjects
 for s, subject in enumerate(subjects):
     print(subject)
+
+    # initialize variables if first subject
+    if s == 0:
+        res = 20
+        trial_proportion = np.zeros([len(subjects),29,29,res])
+
     # load individual data
-    # XXX contrast[0]['include']['cond'] should be changed into contrast[0]['name']
     path_x = op.join(results_path, subject, 'mvpas',
         '{}-decod_{}_{}.pickle'.format(subject, contrast[0]['name'], 'SVR'))
 
@@ -57,19 +58,19 @@ for s, subject in enumerate(subjects):
     trial_proportion[s,:,:,:] = trial_prop
 
 # plot average tuning curve across subjects on the diagonal
-trial_prop_diag = np.array([trial_proportion[:,t,t,:] for t in np.arange(trial_proportion.shape[1])])
+trial_prop_diag = np.array([trial_proportion[:,t,t,:]
+                                for t in np.arange(trial_proportion.shape[1])])
 trial_prop_diag = trial_prop_diag.transpose([1,2,0])
 
+plt.figure()
 plt.imshow(trial_prop_diag.mean(axis=0), interpolation='none', origin='lower')
-
+plt.colorbar()
 
 #-----------------------SVC-----------------------------------------------------
-# classifier type is SVR
+# classifier type is SVC
 clf_type = clf_types[0]
-
 # contrast is target orientation (for now...)
 contrast = clf_type['contrasts'][0]
-
 
 for s, subject in enumerate(subjects):
     print(subject)
@@ -88,12 +89,16 @@ for s, subject in enumerate(subjects):
     # initialize across subjects array if first subject
     if s == 0:
         dims = np.array(shape(probas))
-        PROBAS_diag = np.zeros(np.append(len(subjects),
-                                            dims[[1,3]]))
+        tuning_diag = np.zeros(np.append(len(subjects), dims[[1,3]]))
+        #PROBAS = np.zeros(np.append(len(subjects),dims))
 
     # store individual data into across data array
-    PROBAS_diag[s,:,:] = np.mean([probas[t,t,:,:] for t in arange(29)],axis=1)
+    #PROBAS[s,:,:,:] = np.append(np.mean(probas, axis = 2))
+    # store tuning curve along diagonal
+    tuning_diag[s,:,:] = np.mean([probas[t,t,:,:] for t in arange(29)],axis=1)
 
 # plot average tuning curve across subjects on diagonal
-PROBAS_diag=PROBAS_diag.transpose([0,2,1])
-plt.imshow(np.mean(PROBAS_diag,axis=0),interpolation='none')
+tuning_diag=tuning_diag.transpose([0,2,1])
+plt.figure()
+plt.imshow(np.mean(tuning_diag,axis=0),interpolation='none')
+plt.colorbar()
