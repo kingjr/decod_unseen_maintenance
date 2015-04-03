3b7c9ce8309d76ab7e47eb7876da44a34f792484
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-04-03 20:24:33 +0100

    simplified recombine svr prediction. Now it does not compute trial proportions for tuning curve plotting

--- python/postproc_functions.py
+++ python/postproc_functions.py
@@ -8,90 +8,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def recombine_svr_prediction(gatx,gaty, res=40):
+def recombine_svr_prediction(gatx, gaty):
     """
     This function takes the paths of two classifiers SVR predictions, typically
     sine and cosine of an angle, and combine them into a predicted angle in
     radians
     """
 
-    # The way it is done in Matlab XXX to be removed
-    #
-    # % realign to get single tuning curve across angles
-    # predict_error = [];
-    # for a = 6:-1:1
-    #     % select trials with angles a
-    #     sel = angles==a;
-    #     % relign across angle categories
-    #     predict_error(sel,:,:) = mod((pi+mod(theta(sel,:,:),2*pi)-2*deg2rad(-15+30*a))/2,pi);
-    # end
-    # sel = isnan(angles);
-    # predict_error(sel,:,:) = NaN;
-    #
-    #
-    # % compute proportion of trials correctly predicted
-    # trial_proportion = hist(predict_error-pi/2,borns(-pi/2,pi/2,res));
-    # trial_proportion = reshape(trial_proportion,[size(trial_proportion,1) size(predict_error,2) size(predict_error,3)]);
-    # trial_proportion(1,:,:) = trial_proportion(1,:,:)+trial_proportion(end,:,:);
-    # trial_proportion(end,:,:) = trial_proportion(1,:,:);
-    # % get proportion of trials
-    # trial_proportion = trial_proportion./repmat(sum(trial_proportion),[size(trial_proportion,1),1,1]);
+    pi = np.pi
 
-    # define angles in degrees
-    angles = np.deg2rad(np.linspace(15, 165, 6))
+    # get true angle
+    true_x = gatx.y_train_
+    true_y = gaty.y_train_
+    true_angle, _ = cart2pol(true_x, true_y)
+    true_angle = np.squeeze(true_angle)
 
-    #load first regressor (cosine)
-    x = np.array(gatx.y_pred_)
+    # get x and y regressors (cos and sin)
+    predict_x = np.array(gatx.y_pred_)
+    predict_y = np.array(gaty.y_pred_)
+    predict_angles, _ = cart2pol(predict_x, predict_y)
+    predict_angles = np.squeeze(predict_angles)
 
-    #load second regressor (sine)
-    y = np.array(gaty.y_pred_)
+    # compute angle error
+    n_T = len(gatx.train_times['times_'])
+    n_t = len(gatx.test_times_['times_'][0])
+    true_angles = np.tile(true_angle, (n_T, n_t, 1))
+    angle_errors = (predict_angles - true_angles + pi) % (2 * pi) - pi
 
-    # cartesian 2 polar (radians) transformation
-    theta, _ = cart2pol(x, y)
-
-    # take only values within 0 to 2 pi radians range
-    predict_angle = (np.squeeze(theta) % (2 * np.pi))
-
-    # true angle in degrees
-    # XXX CAREFUL, HERE IT WORKS BECAUSE ORIENTATIONS GO FROM 0 to 180
-    # retrieve true predictor (eg. true cosine)
-    trueX = gatx.y_train_# try to remove from return?
-    true_angle = np.arccos(trueX)
-
-    # realign to get single tuning curve across angles
-    predict_error = np.zeros(predict_angle.shape)
-    r = lambda i: np.round(100000 * i) / 100000  # to avoid float error
-    for a in angles:
-        # select trials with angles a
-        # XXX this is soon to be replaced by the new classification.
-        sel = r(true_angle) == r(a)
-        # relign across angle categories
-        predict_error[:,:,sel] = ((predict_angle[:, :, sel] - 2 * a) / 2) % np.pi
-
-    # define bin_edges
-    bin_edges = lambda m, M, n: np.arange(m+(M-m)/n/2,(M+(M-m)/n/2),(M-m)/n)
-    # compute proportion of trials correctly predicted
-    N = histogramnd(predict_error.squeeze() - np.pi/2,
-                                    bins=bin_edges(-np.pi/2,np.pi/2,res+1),
-                                    axis=2)
-    # extract frequencies
-    trial_freq = N[0]
-
-    # Wrap around first and last bin if identical
-    if False:
-        trial_freq[1,:,:] = trial_freq[1,:,:]+trial_freq[-1,:,:]
-        trial_freq[-1,:,:] = trial_freq[1,:,:]
-
-    #compute totals
-    totals = np.tile(np.sum(trial_freq,axis=2),[res,1,1])
-    # compute the proportion of trials in each bin
-    trial_prop = trial_freq / totals.astype(np.float).transpose([1, 2, 0])
-
-    return (theta, trueX, trial_prop)
+    return predict_angles, true_angle, angle_errors
 
 def cart2pol(x, y):
-    radius = np.sqrt(x ** 2 + y ** 2)
     theta = np.arctan2(y, x)
+    radius = np.sqrt(x ** 2 + y ** 2)
     return(theta, radius)
 
 def realign_angle(gat, angles = [15, 45, 75, 105, 135, 165] ):
