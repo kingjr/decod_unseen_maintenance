a3fa396f346ff0c07306d49af0e2d7a32b2e3f1e
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-04-06 14:00:40 +0100

    bug fixes for angle error computations in SVR. Plus split across visibilities - not working properly yet

--- python/temp/orientation_stats_across.py
+++ python/temp/orientation_stats_across.py
@@ -26,11 +26,13 @@ from postproc_functions import (realign_angle,
                                 plot_circ_hist
 )
 
+"""
 # ------------------------------------------------------------------------------
 # ------------------------------------SVC---------------------------------------
 # This is the stats across trials, but really we only need to apply a similar
 # thing across subjects. This is done in orientation_stats_across.py
-Z, V, p_values_v, p_values_z, angle_errors, angle_errors_vis = list(), list(), list(), list(), list(), list()
+"""
+Z, V, p_values_v, p_values_z, angle_errors = list(), list(), list(), list(), list()
 for s, subject in enumerate(subjects):
     print(subject)
 
@@ -44,7 +46,7 @@ for s, subject in enumerate(subjects):
 
     # read GAT data
     with open(path_gat) as f:
-        gat, contrast, _, events = pickle.load(f)
+        gat, contrast, sel, events = pickle.load(f)
 
     n_time, n_test_time, n_trials, n_categories = np.shape(gat.y_pred_)
 
@@ -85,17 +87,18 @@ for s, subject in enumerate(subjects):
     angle_errors.append(np.mean(angle_error, axis=2))
 
     # divide by visibility
-    for vis in [range(4) + 1]:
-        angle_errors_vis.append(np.mean(
-                angle_error[:,:,events['response_visibilityCode']==vis],axis=2))
+    if s ==0:
+        angle_errors_vis = np.zeros([len(subjects), n_time, n_test_time, 4])
+    for v, vis in enumerate(arange(1, 5)):
+        angle_errors_vis[s,:,:,v] = np.mean(angle_error[:,:,events['response_visibilityCode'][sel]==vis],axis=2)
 
 
 # A)
 # plot error, ie class distance
-plt.subplot(221)
-plt.imshow(mean(angle_errors,axis=0),interpolation='none',origin='lower')
-plt.colorbar()
-plt.title('weighted negative error')
+# plt.subplot(221)
+# plt.imshow(mean(angle_errors,axis=0),interpolation='none',origin='lower')
+# plt.colorbar()
+# plt.title('weighted negative error')
 
 # B)
 # define X
@@ -126,7 +129,7 @@ x, y = np.meshgrid(gat.train_times['times_'],
 
 # PLOT
 # ------ Plot GAT
-gat.scores_ = np.mean(angle_errors, axis=0)
+gat.scores_ = np.mean(np.array(angle_errors), axis=0)
 fig = gat.plot(vmin=np.min(gat.scores_), vmax=np.max(gat.scores_),
                show=False)
 ax = fig.axes[0]
@@ -150,8 +153,69 @@ ax.set_xlabel('Time (s)')
 ax.set_ylabel('Angle error')
 plt.show()
 
+""" Divide by visibility"""
+# -- Divide by visibility
+for vis in arange(4):
+    # define X
+    X = np.array(angle_errors_vis[0:19,:,:,vis]) - np.pi /6.
+
+    # bound to common scale
+    lims = [np.min(np.mean(angle_errors_vis,axis=0)),
+            np.max(np.mean(angle_errors_vis,axis=0))]
+
+    # ------ Run stats
+    T_obs_, clusters, p_values, _ = spatio_temporal_cluster_1samp_test(
+                                           X,
+                                           out_type='mask',
+                                           n_permutations=n_permutations,
+                                           connectivity=None,
+                                           threshold=threshold,
+                                           n_jobs=-1)
+
+    # ------ combine clusters and retrieve min p_values for each feature
+    p_values = np.min(np.logical_not(clusters) +
+                      [clusters[c] * p for c, p in enumerate(p_values)],
+                      axis=0)
+    x, y = np.meshgrid(gat.train_times['times_'],
+                       gat.test_times_['times_'][0],
+                       copy=False, indexing='xy')
+
+
+    # PLOT
+    # ------ Plot GAT
+    gat.scores_ = np.mean(angle_errors_vis[0:19,:,:,vis], axis=0)
+    gat.plot(vmin=lims[0], vmax=lims[1],
+                   show=False, title=vis)
+    plt.contour(x, y, p_values < alpha, colors='black', levels=[0])
+    # plt.xlabel('Test time')
+    # plt.ylabel('Train time')
+    plt.show()
+
+    # ------ Plot Decoding
+    plt.figure(2)
+    times = gat.train_times['times_']
+    scores_diag = np.transpose([angle_errors_vis[0:19, t, t,vis] for t in range(len(times))])
+    plt.subplot(4,1,vis+1)
+    ax = plt.gca()
+    plot_eb(times, np.mean(scores_diag, axis=0),
+            np.std(scores_diag, axis=0) / np.sqrt(scores_diag.shape[0]),
+            color='blue', ax=ax)
+    ymin, ymax = ax.get_ylim()
+    sig_times = times[np.where(np.diag(p_values) < alpha)[0]]
+    sfreq = (times[1] - times[0]) / 1000
+    fill_betweenx_discontinuous(ax, ymin, ymax, sig_times, freq=sfreq,
+                                color='gray', alpha=.3)
+    ax.axhline(np.pi/6, color='k', linestyle='--', label="Chance level")
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Angle error (radians)')
+    plt.show()
+
+
+
+"""
 # ------------------------------------------------------------------------------
 # -------------------------------SVR--------------------------------------------
+"""
 Z, V, p_values_v, p_values_z, angle_errors, angle_errors_vis = list(), list(), list(), list(), list(), list()
 for s, subject in enumerate(subjects):
     print(subject)
@@ -190,18 +254,18 @@ for s, subject in enumerate(subjects):
     Z.append(z)
     p_values_v.append(p_val_v)
     p_values_z.append(p_val_z)
-    angle_errors.append(np.mean(angle_error, axis=2))
+    angle_errors.append(np.mean(angle_error ** 2, axis=2))
 
     # divide by visibility
     if s ==0:
         angle_errors_vis = np.zeros([len(subjects), n_time, n_test_time, 4])
     for v, vis in enumerate(arange(1, 5)):
-        angle_errors_vis[s,:,:,v] = np.mean(angle_error[:,:,events['response_visibilityCode'][sel]==vis],axis=2)
+        angle_errors_vis[s,:,:,v] = np.mean(angle_error[:,:,events['response_visibilityCode'][sel]==vis] ** 2,axis=2)
 
 
 # define X
 angle_errors = np.array(angle_errors)
-X = angle_errors - np.pi /2.
+X = angle_errors - np.pi /6.
 # use last loaded gat object as a template
 gat = gaty
 
@@ -261,7 +325,7 @@ plt.show()
 # -- Divide by visibility
 for vis in arange(4):
     # define X
-    X = angle_errors_vis[:,:,:,vis] - np.pi /2.
+    X = angle_errors_vis[:,:,:,vis] - np.pi /6.
 
     # ------ Run stats
     T_obs_, clusters, p_values, _ = spatio_temporal_cluster_1samp_test(
