021c171955ef499c7d4e06bcce40bdca77298b9c
chri4354 <niccolo.pescetelli@psy.ox.ac.uk>
2015-04-09 17:43:37 +0100

    FIX: stats functions of orientation simplification

--- python/orientation_stats_across.py
+++ python/orientation_stats_across.py
@@ -20,17 +20,18 @@ from mne.stats import spatio_temporal_cluster_1samp_test
 
 from config import (subjects, results_path, clf_types)
 from toolbox.utils import (plot_eb, fill_betweenx_discontinuous)
-from postproc_functions import (
-                                realign_angle,
+from postproc_functions import (realign_angle,
                                 cart2pol,
                                 recombine_svr_prediction,
+                                compute_error_svc,
+                                compute_error_svr,
                                 plot_circ_hist,
-                                cluster_test_main
-)
+                                cluster_test_main)
 
 
 for clf_type in clf_types:
-    Z, V, p_values_v, p_values_z, angle_errors = list(), list(), list(), list(), list()
+    #Z, V, p_values_v, p_values_z = list(), list(), list(), list()
+    angle_errors, angle_errors_vis = list(), list() * 10
     for s, subject in enumerate(subjects):
         print(subject)
 
@@ -45,29 +46,13 @@ for clf_type in clf_types:
 
             # read GAT data
             with open(path_gat) as f:
-                gatx, contrast, sel, events = pickle.load(f)
+                gat, contrast, sel, events = pickle.load(f)
 
-            n_time, n_test_time, n_trials, n_categories = np.shape(gatx.y_pred_)
+            n_time, n_test_time, n_trials, n_categories = np.shape(gat.y_pred_)
 
-            # realign to 0th angle category
-            probas = realign_angle(gatx)
+            # get angle error from probas
+            angle_error = compute_error_svc(gat)
 
-            # transform 6 categories into single angle: there are two options here,
-            # try it on the pilot subject, and use weighted mean if the two are
-            # equivalent
-            weighted_mean = True
-            if weighted_mean:
-                # multiply category ind (1, 2, 3, ) by angle, remove pi to be between
-                # -pi and pi and remove pi/6 to center on 0
-                operator = np.arange(n_categories) * np.pi / 3 + np.pi / 6
-                operator = np.tile(operator, (n_time, n_time, n_trials, 1))
-                # average angles: XXX THEORETICALLY INCORRECT
-                angle_error = np.mean(probas * operator, axis=3)
-                # XXX NB we should average in complex space but somehow it doesnt work
-                #xy = np.mean(probas * (np.cos(operator) + 1j *sin(operator)), axis=3)
-                #angle_error, _ = cart2pol(real(xy), imag(xy))
-            else:
-                angle_error = np.argmax(probas, axis=3) * np.pi / 3 - np.pi / 6
         elif clf_type['name']=='SVR':
             # define results path
             path_x = op.join(results_path, subject, 'mvpas',
@@ -87,37 +72,38 @@ for clf_type in clf_types:
             n_time, n_test_time, n_trials, n_categories = np.shape(gaty.y_pred_)
 
             # recombine cos and sin predictions into one predicted angle
-            predict_angle, true_angle, angle_error = recombine_svr_prediction(gatx, gaty)
+            predict_angle, true_angle = recombine_svr_prediction(gatx, gaty)
+
+            # compute angle error
+            angle_error = compute_error_svr(predict_angle, true_angle)
+
+            # Keep gat for later plotting purposes
+            gat = gatx
 
 
         ####### STATS WITHIN SUBJECTS
         # Apply v test and retrieve statistics that is independent of the number of
         # trials.
-        p_val_v, v = vtest(angle_error, 0, axis=2) # trials x time
-
-        # apply Rayleigh test (better for of diagonal with distribution shifts, e.g.
-        # on the n200, the prediction may reverse)
-        p_val_z, z = rayleigh(angle_error, axis=2) # trials x time
-
-        # append scores
-        V.append(v)
-        Z.append(z)
-        p_values_v.append(p_val_v)
-        p_values_z.append(p_val_z)
-        if clf_type['name']=='SVC':
-            angle_errors.append(np.mean(angle_error, axis=2))
-        elif clf_type['name']=='SVR':
-            angle_errors.append(np.mean(angle_error ** 2, axis=2))
+        # p_val_v, v = vtest(angle_error, 0, axis=2) # trials x time
+        #
+        # # apply Rayleigh test (better for of diagonal with distribution shifts, e.g.
+        # # on the n200, the prediction may reverse)
+        # p_val_z, z = rayleigh(angle_error, axis=2) # trials x time
+        #
+        # # append scores
+        # V.append(v)
+        # Z.append(z)
+        # p_values_v.append(p_val_v)
+        # p_values_z.append(p_val_z)
+        angle_errors.append(np.mean(angle_error, axis=2))
 
         # divide by visibility
-        if s ==0:
-            angle_errors_vis = np.zeros([len(subjects), n_time, n_test_time, 4])
-        for v, vis in enumerate(range(1, 5)):
-            indx = np.array(events['response_visibilityCode'][sel]==vis)
-            if clf_type['name']=='SVC':
-                angle_errors_vis[s,:,:,v] = np.mean(angle_error[:,:,indx],axis=2)
-            elif clf_type['name']=='SVR':
-                angle_errors_vis[s,:,:,v] = np.mean(angle_error[:,:,indx] ** 2,axis=2)
+        angle_errors_vis_ = list()
+        visibilities = range(1, 5)
+        for v, vis in enumerate(visibilities):
+            indx = np.where(events['response_visibilityCode'][sel]==vis)[0]
+            angle_errors_vis_.append(np.mean(angle_error[:,:,indx],axis=2))
+        angle_errors_vis.append(angle_errors_vis_)
 
 
     # A)
@@ -129,22 +115,20 @@ for clf_type in clf_types:
 
     # B)
     # define X
-    baseline = np.pi / 6.
+    chance_level = np.pi / 6.
 
     # perform cluster test on main effect (difference from chance)
-    cluster_test_main(gatx, angle_errors, baseline, ylabel='Angle error')
+    cluster_test_main(gat, angle_errors, chance_level, ylabel='Angle error')
 
-    """ Divide by visibility"""
-    # -- Divide by visibility
-    for vis in range(4):
-        # define A (non corrected measure)
-        if 'SVC':
-            A = np.array(angle_errors_vis[0:19,:,:,vis])
-        elif 'SVR':
-            A = np.array(angle_errors_vis[0:19,:,:,vis]) ** 2
+    # Test wether decoding is significant at eahc visibility level
+    for v, vis in enumerate(visibilities):
+        # define A (non corrected measure) XXX JRK : to be solved later: last subject doesnt have unseen trials
+        A = np.array(angle_errors_vis)[0:19, v, :, :]
 
         # bound to common scale
         lims = [np.min(A), np.max(A)]
 
-        cluster_test_main(gatx, A, baseline, ylabel='Angle error', title=vis,
+        cluster_test_main(gat, A, chance_level, ylabel='Angle error', title=vis,
                             lims=lims)
+
+    # XXX add saveing
