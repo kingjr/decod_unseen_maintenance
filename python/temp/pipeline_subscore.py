8cd998375a5d7ee55b47721070086faa857371fb
Niccolo <niccolo.pescetelli@psy.ox.ac.uk>
2015-05-15 19:35:09 +0100

    remove generalization and combination of gats

--- python/temp/pipeline_subscore.py
+++ python/temp/pipeline_subscore.py
@@ -30,45 +30,6 @@ def pkl_fname(type, subject, contrast):
         '{}-decod_{}{}.pickle'.format(
             subject, contrast['name'], fname_appendix))
 
-def combine_gat_y(gat_list, order_list=None):
-    """Combines multiple gat.y_pred_ & gat.y_train_ into a single gat.
-
-    Parameters
-    ----------
-        gat_list : list of GeneralizationAcrossTime objects, shape (n_gat)
-            The gats must have been predicted (gat.predict(epochs))
-        order_list : None | list, shape (n_gat), optional
-            Order of the prediction, to be recombined. Defaults to None.
-    Returns
-    -------
-        gat : GeneralizationAcrossTime object"""
-
-    if order_list is not None:
-        if len(gat_list) != len(order_list):
-            raise ValueError('len(order_list) must equal len(gat_list)')
-    else:
-        order = [range(len(gat.y_pred_[0][0])) for gat in gat_list]
-        for idx in range(1, len(order)):
-            order[idx] += len(order[idx-1])
-    # Identifiy trial number
-    n_trials = np.max([np.max(sel) for sel in order_list])
-    n_dims = np.shape(gat_list[0].y_pred_[0][0])[1]
-    # initialize combined gat
-    cmb_gat = gat_list[0]
-    # initialize y_pred
-    for train in range(gat.y_pred_):
-        for test in range(gat.y_pred_[train]):
-            cmb_gat.y_pred_[train][test] = np.nan * np.ones((n_trials, n_dims))
-    # initialize y_train
-    cmb_gat.y_train_ = np.ones((n_trials,))
-
-    for gat, sel in zip(gat_list, order_list):
-        for train in range(gat.y_pred_):
-            for test in range(gat.y_pred_[train]):
-                cmb_gat.y_pred_[train][test][sel, :] = gat.y_pred_[train][test]
-        cmb_gat.y_train_[sel] = gat.y_train_
-    return cmb_gat
-
 
 def subscore(gat, sel, y=None, scorer=None):
     """Subscores a GAT.
@@ -137,18 +98,9 @@ for ep in epochs_params:
         y_pred_list = list()
         for subject in subjects:
             # Load CV data
-            if analysis['contrast'] is not None:
-                file = pkl_fname(typ, subject, analysis['contrast'])
-                with open(file) as f:
-                    gat_dec, _, events, sel_dec = pickle.load(f)
-            # Load Generalize data
-            if analysis['generalization'] is not None:
-                file = pkl_fname(typ, subject, analysis['contrast'])
-                with open(file) as f:
-                    gat_gen, _, events, sel_gen = pickle.load(f)
-
-            # Combine multiple gat predictions
-            gat = combine_gat_y([gat_dec, gat_gen], [sel_dec, sel_gen])
+            file = pkl_fname(typ, subject, analysis['contrast'])
+            with open(file) as f:
+                gat, _, events, sel = pickle.load(f)
 
             # Subscore overall
             sel = find_in_df(events, analysis['include'], analysis['exclude'])
