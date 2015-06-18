94b76201ce0d418191026a0fa5bc9846f51298f4
kingjr <jeanremi.king+github@gmail.com>
2015-06-18 14:09:04 -0400

    combine utils.py

--- orientations/utils.py
+++ orientations/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 
+from toolbox.utils import evoked_subtract, evoked_spearman  # , evoked_vtest
+
 
 def angle2circle(angles):
     """from degree to radians multipled by 2"""
@@ -225,3 +227,35 @@ def scorer_spearman(y_true, y_pred):
     from scipy.stats import spearmanr
     rho, p = spearmanr(y_true, y_pred[:, 0])
     return rho
+
+
+def format_analysis(contrast):
+    """This functions takes the contrasts defined for decoding  and format it
+    so as to be usable by the univariate scripts
+
+    We need to homogeneize the two types of analysis definitions
+     """
+    name = contrast['name']
+    if contrast['scorer'] == scorer_spearman:
+        operator = evoked_spearman
+    elif contrast['scorer'] == scorer_auc:
+        operator = evoked_subtract
+    elif contrast['scorer'] == scorer_angle:
+        # TODO evoked_vtest
+        return
+    # exclude
+    exclude = dict()
+    for exclude_ in contrast['exclude']:
+        cond = exclude_['cond']
+        exclude[cond] = exclude_['values']
+
+    # include
+    conditions = list()
+    cond = contrast['include']['cond']
+    for value in contrast['include']['values']:
+        include_ = dict()
+        include_[cond] = value
+        conditions.append(dict(name=cond + str(value), include=include_,
+                               exclude=exclude))
+    analysis = dict(name=name, operator=operator, conditions=conditions)
+    return analysis
