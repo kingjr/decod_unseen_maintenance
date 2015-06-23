aa175c804110242dc0f3a7776d5da8f809d31ffe
kingjr <jeanremi.king+github@gmail.com>
2015-06-22 20:22:45 -0400

    EHN: upload download dropbox s3

--- aws/load_from_dropbox.py
+++ aws/load_from_dropbox.py
@@ -1,12 +1,18 @@
-from aws.utils import dropbox_download
 from scripts.config import subjects, paths
 
+from aws.utils import (upload, download)
+
+import os.path as op
+
 for subject in subjects:
+    subject = subjects[0]
     # behavior
     f_local = paths('behavior', subject=subject)
-    f_dropbox = f_local.split('/')[-1]
-    dropbox_download(f_dropbox, f_local)
-    # epoch light
-    f_local = paths('epoch', subject=subject, data_type='erf')
-    f_dropbox = subject + '_preprocessed.mat'
-    dropbox_download(f_dropbox, f_local)
+    f_dropbox = op.join('/no_sync/niccolo_data/', f_local.split('/')[-1])
+    download('dropbox', f_dropbox, f_local)
+    upload('s3', f_local, f_local)
+    # # epoch light
+    # f_local = paths('epoch', subject=subject, data_type='erf')
+    # f_dropbox = subject + '_preprocessed.mat'
+    # download('dropbox', f_dropbox, f_local)
+    # upload('s3', f_local, f_local)
--- aws/utils.py
+++ aws/utils.py
@@ -1,34 +1,75 @@
+import warnings
+import os
 import os.path as op
+import boto
+import dropbox
+from os.path import expanduser
+
+credentials_dir = op.join(expanduser("~"), '.credentials')
+with open('%s/boto.cfg' % credentials_dir, 'rb') as f:
+    auth = f.read()
+    auth = auth.split('\n')
+    AWSAccessKeyId = auth[0].split('AWSAccessKeyId=')[1]
+    AWSSecretKey = auth[1].split('AWSSecretKey=')[1]
+boto_client = boto.connect_s3(
+    AWSAccessKeyId, AWSSecretKey,
+    calling_format='boto.s3.connection.OrdinaryCallingFormat').get_bucket(
+        'meg.niccolo')
+print('connected to S3')
+with open('%s/dropbox.pem' % credentials_dir, 'rb') as f:
+    auth = f.read()
+dropbox_client = dropbox.client.DropboxClient(auth.split('\n')[0])
+print('connected to dropbox')
 
 
-def dropbox_client():
-    import dropbox
-    # read credentials
-    with open('/home/jrking/credentials/dropbox.pem', 'rb') as f:
-        auth = f.read()
-    # connect to account
-    client = dropbox.client.DropboxClient(auth.split('\n')[0])
-    # print 'linked account: ', client.account_info()
-    base_path = '/no_sync/niccolo_data/'
-    return client, base_path
+def download(server, f_server, f_client, overwrite=False):
+    # Deal with inexistent directories
+    this_path = '/'.join(f_client.split('/')[:-1])
+    if not op.exists(this_path):
+        os.makedirs(this_path)
+
+    if server == 'dropbox':
+        f, metadata = dropbox_client.get_file_and_metadata(f_server)
+        if op.exists(f_client):
+            warnings.warn('%s already exists.' % f_client)
+            if not overwrite:
+                warnings.warn('%s was not overwritten.' % f_client)
+                return
+        out = open(f_client, 'wb')
+        out.write(f.read())
+        out.close()
+    elif server == 's3':
+        key = boto_client.get_key(f_server)
+        if key is None:
+            raise RuntimeError('%s is not online.' % f_client)
+        # Check for overwriting
+        if op.exists(f_client):
+            warnings.warn('%s already exists.' % f_client)
+            if not overwrite:
+                warnings.warn('%s was not overwritten.' % f_client)
+                return
+        key.get_contents_to_filename(f_client)
+    else:
+        raise RuntimeError('unknown server %s' % server)
+    print('downloaded from %s: %s > %s' % (server, f_server, f_client))
 
 
-def dropbox_download(f_server, f_client=None):
-    f_client = f_server if f_client is None else f_client
-    client, base_path = dropbox_client()
-    f_server = op.join(base_path, f_server)
-    f, metadata = client.get_file_and_metadata(f_server)
-    out = open(f_client, 'wb')
-    out.write(f.read())
-    out.close()
-    print('downloadded: %s > %s' % (f_server, f_client))
-
-
-def dropbox_upload(f_client, f_server=None):
-    f_server = f_client if f_server is None else f_client
-    client, base_path = dropbox_client()
-    f = open(f_client, 'rb')
-    f_server = op.join(base_path, f_server)
-    response = client.put_file(op.join(base_path, f_client), f)
-    print 'uploaded: ', response
-    print('downloaded: %s > %s' % (f_server, f_client))
+def upload(server, f_client, f_server, overwrite=False):
+    if not op.exists(f_client):
+        raise RuntimeError('%s does not exist.' % f_client)
+    if server == 'dropbox':
+        f = open(f_client, 'rb')
+        response = dropbox_client.put_file(f_server, f)
+        print response
+        # FIXME overwrite
+    elif server == 's3':
+        key = boto_client.get_key(f_server)
+        if key is None:
+            key = boto_client.new_key(f_server)
+        elif not overwrite:
+            raise RuntimeError('%s already exists online. '
+                               'Set overwrite=True.' % f_server)
+        key.set_contents_from_filename(f_server)
+    else:
+        raise RuntimeError('unknown server %s' % server)
+    print('uploaded to %s > %s: %s' % (server, f_server, f_client))
