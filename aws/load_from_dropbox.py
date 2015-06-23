b82b720c8f85c2e9042258e8dcb800a01216d584
kingjr <jeanremi.king+github@gmail.com>
2015-06-23 08:36:28 -0400

    load epoch data on s3

--- aws/load_from_dropbox.py
+++ aws/load_from_dropbox.py
@@ -2,17 +2,30 @@ from scripts.config import subjects, paths
 
 from aws.utils import (upload, download)
 
+import os
 import os.path as op
 
 for subject in subjects:
-    subject = subjects[0]
     # behavior
     f_local = paths('behavior', subject=subject)
-    f_dropbox = op.join('/no_sync/niccolo_data/', f_local.split('/')[-1])
+    f_dropbox = op.join('/no_sync/niccolo_data/',
+                        f_local.split('/')[-1])
+    download('dropbox', f_dropbox, f_local, True)
+    upload('s3', f_local, f_local, True)
+
+    # epoch light
+    f_local = paths('epoch', subject=subject, data_type='erf')
+    f_dropbox = op.join('/no_sync/niccolo_data/',
+                        subject + '_preprocessed.mat')
     download('dropbox', f_dropbox, f_local)
-    upload('s3', f_local, f_local)
-    # # epoch light
-    # f_local = paths('epoch', subject=subject, data_type='erf')
-    # f_dropbox = subject + '_preprocessed.mat'
-    # download('dropbox', f_dropbox, f_local)
-    # upload('s3', f_local, f_local)
+    upload('s3', f_local, f_local, True)
+
+    # epoch heavy
+    f_local = paths('epoch', subject=subject, data_type='erf')
+    f_local = f_local[:-4] + '.dat'
+    f_dropbox = op.join('/no_sync/niccolo_data/',
+                        subject + '_preprocessed.dat')
+    download('dropbox', f_dropbox, f_local)
+    upload('s3', f_local, f_local, True)
+    if subject in subjects[1:]:
+        os.remove(f_local)
