008679ebb6d7cfd229530a917e1dd6652ef18c5d
kingjr <jeanremi.king+github@gmail.com>
2015-06-23 13:50:27 -0400

    add download behavior

--- scripts/transfer_data.py
+++ scripts/transfer_data.py
@@ -1,22 +1,29 @@
 import sys
 sys.path.insert(0, './')
 
-def download_depencies(overwrite=False):
+
+def download_all(overwrite=False):
     from itertools import product
     from cloud.utils import download
     # XXX This will have to be specified for later scripts
     from scripts.config import paths, subjects, data_types
 
     file_list = list()
+    # behavior
+    for subject in subjects:
+        file_list.append(paths('behavior', subject=subject))
+    # epoch
     for subject, data_type in product(subjects, data_types):
-        file_list.append(paths('epoch', subject=subject, data_type=data_type))
+        fname = paths('epoch', subject=subject, data_type=data_type)
+        file_list.append(fname)
+        file_list.append(fname[:-4] + '.dat')
 
     for fname in file_list:
         for fname in file_list:
             download('s3', fname, fname, overwrite)
 
 
-def upload_results(overwrite=False):
+def upload_all(overwrite=False):
     from scripts.config import paths
     from cloud.utils import upload
 
