2695eb0e79e8b72bfd8e1a1f465b5800a4a99b1a
kingjr <jeanremi.king+github@gmail.com>
2015-09-30 09:18:34 -0400

    EHN: add phase analyses

--- orientations/conditions.py
+++ orientations/conditions.py
@@ -1,4 +1,5 @@
 # Decoding parameters
+import matplotlib.pyplot as plt
 from itertools import product
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
@@ -8,7 +9,7 @@ from base import scorer_angle, scorer_circLinear, scorer_auc, scorer_spearman
 
 
 def analysis(name, typ, condition=None, query=None, title=None):
-    single_trial = False
+    # single_trial = False
     erf_function = None
     if typ == 'categorize':
         clf = Pipeline([('scaler', StandardScaler()),
@@ -18,60 +19,63 @@ def analysis(name, typ, condition=None, query=None, title=None):
     elif typ == 'regress':
         clf = Pipeline([('scaler', StandardScaler()), ('svr', LinearSVR(C=1))])
         scorer = scorer_spearman
-        single_trial = True  # with non param need single trial here
+        # single_trial = True  # with non param need single trial here
         chance = 0.
     elif typ == 'circ_regress':
         clf = SVR_angle()
         scorer = scorer_angle
         chance = 0.
-        single_trial = True
+        # single_trial = True
         erf_function = scorer_circLinear
     if condition is None:
         condition = name
     return dict(name=name, condition=condition, query=query, clf=clf,
                 scorer=scorer, chance=chance, erf_function=erf_function,
-                single_trial=single_trial, cv=8, typ=typ, title=title)
+                single_trial=True, cv=8, typ=typ, title=title)
 
 analyses = (
-    analysis('target_present',      'categorize', title='Target Present'),
-    # analysis('target_contrast',     'regress'),
-    analysis('target_contrast_pst', 'regress', condition='target_contrast',
-             query='target_present == True', title='Target Contrast'),
-    analysis('target_spatialFreq',  'categorize',
-             title='Target Spatial Frequency'),
-    analysis('target_circAngle',    'circ_regress', title='Target Angle'),
-    analysis('probe_circAngle',     'circ_regress', title='Probe Angle'),
-    analysis('probe_tilt',          'categorize', title='Target - Probe Tilt'),
-    analysis('discrim_button',      'categorize'),
-    # analysis('discrim_correct',     'categorize'),
-    # analysis('detect_button',       'regress'),
-    analysis('detect_button_pst',   'regress', condition='detect_button',
-             query='target_present == True', title='Visibility Response'),
-    # analysis('detect_seen',         'categorize'),
-    # analysis('detect_seen_pst',     'categorize', condition='detect_seen',
-    #          query='target_present == True',
-    #          title='Tilt Discrimination Response')
+    # analysis('target_present',      'categorize', title='Target Present'),
+    # # analysis('target_contrast',     'regress'),
+    # analysis('target_contrast_pst', 'regress', condition='target_contrast',
+    #          query='target_present == True', title='Target Contrast'),
+    # analysis('target_spatialFreq',  'categorize',
+    #          title='Target Spatial Frequency'),
+    # analysis('target_circAngle',    'circ_regress', title='Target Angle'),
+    analysis('target_phase',    'circ_regress', title='Target Phase'),
+    # analysis('probe_circAngle',     'circ_regress', title='Probe Angle'),
+    # analysis('probe_tilt',          'categorize', title='Target - Probe Tilt'),
+    analysis('probe_phase',    'circ_regress', title='Probe Phase'),
+    # analysis('discrim_button',      'categorize',
+    #          title='Discrimination Response'),
+    # # analysis('discrim_correct',     'categorize'),
+    # # analysis('detect_button',       'regress'),
+    # analysis('detect_button_pst',   'regress', condition='detect_button',
+    #          query='target_present == True', title='Visibility Response'),
+    # # analysis('detect_seen',         'categorize'),
+    # # analysis('detect_seen_pst',     'categorize', condition='detect_seen',
+    # #          query='target_present == True')
 )
+
+cmap = plt.get_cmap('gist_rainbow')
+for ii in range(len(analyses)):
+    analyses[ii]['color'] = cmap(float(ii) / len(analyses))
 
 # ###################### Define subscores #####################################
 
-subscores = [
-    ('seen', 'detect_seen == True'),
-    ('unseen', 'detect_seen == False'),
-    ('correct', 'discrim_correct == True'),
-    ('incorrect', 'discrim_correct == False')
-]
-for pas in [1., 2., 3.]:
-    subscores.append(('pas%s' % pas,
-                      'detect_button == %s' % pas))
+#
+subscores = [('seen', 'detect_seen == True'),
+             ('unseen', 'detect_seen == False')]
+# for pas in [1., 2., 3.]:
+#     subscores.append(('pas%s' % pas,
+#                       'detect_button == %s' % pas))
 for contrast in [0., .5, .75, 1.]:
     subscores.append(('contrast%s' % contrast,
                       'target_contrast == %s' % contrast)),
-for pas, contrast in product([1., 2., 3.], [0., .5, .75, 1.]):
+for pas, contrast in product([0., 1., 2., 3.], [.5, .75, 1.]):
     subscores.append(('pas%s-contrast%s' % (pas, contrast),
                       'detect_button == %s and target_contrast == %s' % (
                       pas, contrast)))
-
+#
 analyses_order2 = [
     analysis('angle_vis', 'categorize', condition=[
         analysis('target_circAngle-seen', 'circ_regress'),
