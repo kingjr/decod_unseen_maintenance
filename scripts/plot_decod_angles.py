4fdd07b7b93491ec20696e8101f00755e7ce1010
kingjr <jeanremi.king+github@gmail.com>
2015-09-30 14:54:54 -0400

    cleanup and separate analysis from plotting angle

--- scripts/plot_decod_angles.py
+++ scripts/plot_decod_angles.py
@@ -3,10 +3,8 @@ import numpy as np
 from scipy.stats import wilcoxon
 import matplotlib.pyplot as plt
 from jr.plot import plot_tuning, bar_sem, pretty_decod
-from jr.stats import circ_tuning, circ_mean, robust_mean
-from jr.utils import align_on_diag
-from scripts.config import paths, subjects, subscores, report, analyses
-from base import stats, get_predict, get_predict_error, angle_acc
+from scripts.config import paths, report, analyses
+from base import stats
 analyses = [analysis for analysis in analyses if analysis['name'] in
             ['target_circAngle', 'probe_circAngle']]
 
@@ -22,107 +20,8 @@ def resample1D(x):
     return x
 
 
-# Gather data
-for analysis in ['target_circAngle', 'probe_circAngle']:
-    results = dict(diagonal=list(), angle_pred=list(), toi=list(),
-                   subscore=list(), corr_contrast=list(), corr_pas=list(),
-                   R_contrast=list(), R_vis=list(), align_on_diag=list(),
-                   early_maintain=list())
-    for s, subject in enumerate(subjects):
-        print s
-        fname = paths('decod', subject=subject, analysis=analysis)
-        with open(fname, 'rb') as f:
-            gat, _, events_sel, events = pickle.load(f)
-        times = gat.train_times_['times']
-        subevents = events.iloc[events_sel].reset_index()
-        n_bins = 24
-
-        # Mean error across trial on the diagonal
-        y_error = angle_acc(get_predict_error(gat, mean=False), axis=0)
-        results['diagonal'].append(y_error)
-
-        # Mean prediction for each angle at peak time
-        toi = [.100, .250] if analysis == 'target_circAngle' else [.900, 1.150]
-        results_ = list()
-        for angle in np.unique(gat.y_true_):
-            y_pred = get_predict(gat, sel=np.where(gat.y_true_ == angle)[0],
-                                 toi=toi)
-            probas, bins = circ_tuning(y_pred, n=n_bins)
-            results_.append(probas)
-        results['angle_pred'].append(results_)
-
-        # Mean tuning error per toi
-        results_ = list()
-        for toi in tois:
-            probas, bins = circ_tuning(get_predict_error(gat, toi=toi),
-                                       n=n_bins)
-            results_.append(probas)
-        results['toi'].append(results_)
-
-        # Mean y_error per toi per vis
-        results_ = dict()
-        y_error = get_predict_error(gat, mean=False)
-        for subanalysis in subscores:
-            results_[subanalysis[0] + '_toi'] = list()
-            # subselect events (e.g. seen vs unseen)
-            subsel = subevents.query(subanalysis[1]).index
-            # add nan if no trial matches subconditions
-            if len(subsel) == 0:
-                results_[subanalysis[0]] = np.nan * np.zeros(y_error.shape[1])
-                for toi in tois:
-                    results_[subanalysis[0] + '_toi'].append(np.nan)
-                continue
-            # dynamics of mean error
-            results_[subanalysis[0]] = angle_acc(y_error[subsel, :], axis=0)
-            # mean error per toi
-            for toi in tois:
-                # mean error across time
-                toi_ = np.where((times >= toi[0]) & (times < toi[1]))[0]
-                y_error_toi = circ_mean(y_error[:, toi_], axis=1)
-                y_error_toi = angle_acc(y_error_toi[subsel])
-                results_[subanalysis[0] + '_toi'].append(y_error_toi)
-        results['subscore'].append(results_)
-
-        # Duration
-        results_ = list()
-        for toi in tois:
-            # for each toi: 1. realign pred on diag, 2. compute error
-            y_error = get_predict_error(gat, toi=toi, typ='align_on_diag',
-                                        mean=False)
-            results_pas = list()
-            for pas in range(4):
-                # select condition
-                pas_cond = 'detect_button == %i' % pas
-                present_cond = ' and target_present == True'
-                sel = subevents.query(pas_cond + present_cond).index
-                # if no trial matches seleted conditions, add NaNs
-                if len(sel) == 0:
-                    results_pas.append(np.nan * np.zeros(y_error.shape[2]))
-                    continue
-                # 3. mean error across trials
-                y_error_ = angle_acc(y_error[sel, ...], axis=0)
-                # 4. mean across classifier (toi)
-                y_error_ = np.mean(y_error_, axis=0)
-                results_pas.append(y_error_)
-            results_.append(results_pas)
-        results['align_on_diag'].append(results_)
-
-        # maintenance of early classifiers
-        results_ = list()
-        for toi in [[.100, .150], [.170, .220]]:
-            y_error = get_predict_error(gat, toi=toi, typ='gat')
-            results_.append(angle_acc(y_error, axis=0))
-        results['early_maintain'].append(results_)
-
-    results['times'] = times
-    results['bins'] = bins
-    fname = paths('score', subject='fsaverage', analysis=analysis + '-tuning')
-    with open(fname, 'wb') as f:
-        pickle.dump(results, f)
-
-
-# Plot
 def plot_tuning_(data, ax, color, polar=True):
+    '''facilitate the tuning'''
     shift = None if polar is True else np.pi
     plot_tuning(np.mean(data, axis=0), ax=ax, color=color, polar=polar,
                 half=polar, shift=shift, alpha=.75)
@@ -135,19 +34,35 @@ def plot_tuning_(data, ax, color, polar=True):
         ax.set_xticks([-np.pi, np.pi])
         ax.set_xticklabels(['$-\pi/2$', '$\pi/2$'])
 
+
+def quick_stats(x, ax=None):
+    """returns wilcoxon stats and plot stars where p<.05"""
+    pvals = [wilcoxon(ii)[1] for ii in x.T]
+    sig = [['', '*'][p < .05] for p in pvals]
+    m = np.mean(x, axis=0)
+    s = np.std(x, axis=0)
+    print(m, s, pvals, sig)
+    if ax is not None:
+        for x_, (y_, sig_) in enumerate(zip(m / 2., sig)):
+            ax.text(x_ + .5, y_, sig_, color='w', weight='bold', size=20,
+                    ha='center', va='center')
+
+
+def get_ylim(data):
+    """get the y-axis limits"""
+    m = np.mean(np.array(data), axis=0)
+    s = np.std(np.array(data), axis=0) / np.sqrt(len(data))
+    return np.min(m-s*1.1), np.max(m+s*1.1)
+
 ylim = [.01, .08]
 for analysis in analyses:
+    # load data
     fname = paths('score', subject='fsaverage',
                   analysis=analysis['name'] + '-tuning')
     with open(fname, 'rb') as f:
         results = pickle.load(f)
     times = results['times']
     n_bins = len(results['bins'])
-
-    def get_ylim(data):
-        m = np.mean(np.array(data), axis=0)
-        s = np.std(np.array(data), axis=0) / np.sqrt(len(data))
-        return np.min(m-s*1.1), np.max(m+s*1.1)
 
     def get_sub(key):
         return np.array([subject[key] for subject in results['subscore']])
@@ -183,17 +98,6 @@ for analysis in analyses:
     # seen versus unseen
     seen_toi = get_sub('seen_toi').T
     unseen_toi = get_sub('unseen_toi').T
-
-    def quick_stats(x, ax=None):
-        pvals = [wilcoxon(ii)[1] for ii in x.T]
-        sig = [['', '*'][p < .05] for p in pvals]
-        m = np.mean(x, axis=0)
-        s = np.std(x, axis=0)
-        print(m, s, pvals, sig)
-        if ax is not None:
-            for x_, (y_, sig_) in enumerate(zip(m / 2., sig)):
-                ax.text(x_ + .5, y_, sig_, color='w', weight='bold', size=20,
-                        ha='center', va='center')
 
     fig, axes = plt.subplots(1, len(tois), figsize=[9, 2])
     for ax, unseen, seen, toi in zip(axes, unseen_toi, seen_toi, tois):
@@ -298,6 +202,5 @@ for analysis in analyses:
         pretty_decod(score, sig=p_val < .05, times=times, ax=ax, color=col,
                      fill=True)
     plt.show()
-    # TODO absent trials control reactivation
 
 report.save()
