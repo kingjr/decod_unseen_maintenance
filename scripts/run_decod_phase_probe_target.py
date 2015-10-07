a0719ca64bc5a5d77e12a8e97dcb8a9eaefa6661
kingjr <jeanremi.king+github@gmail.com>
2015-10-07 10:34:39 -0400

    EHN: control analysis: train on probe phase test on target

---
+++ scripts/run_decod_phase_probe_target.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+from orientations.utils import load_epochs_events
+from jr.gat import subscore
+from jr.plot import pretty_decod
+from base import resample_epochs, decim, stats
+from scripts.config import paths, subjects, preproc, analyses, tois
+
+analysis = [ana for ana in analyses if ana['title'] == 'Target Phase'][0]
+toi = [-.100, .305]
+
+scores = list()
+for s, subject in enumerate(subjects):  # Loop across each subject
+    print(subject)
+    epochs, events = load_epochs_events(subject, paths)
+
+    # preprocess data for memory issue
+    if 'resample' in preproc.keys():
+        epochs = resample_epochs(epochs, preproc['resample'])
+    if 'decim' in preproc.keys():
+        epochs = decim(epochs, preproc['decim'])
+    epochs.crop(toi[0], toi[1])
+
+    # select trials
+    query, condition = analysis['query'], analysis['condition']
+    sel = range(len(events)) if query is None \
+        else events.query(query).index
+    sel = [ii for ii in sel if ~np.isnan(events[condition][sel][ii])]
+    y = np.array(events[condition], dtype=np.float32)
+
+    # Load classifier
+    pkl_fname = paths('decod', subject=subject, analysis='probe_phase')
+    with open(pkl_fname, 'rb') as f:
+        gat, _, sel_gat, _ = pickle.load(f)
+    # only keep estimators after probe onset
+    times = gat.train_times_['times']
+    toi_ = np.where((times >= (.800 + toi[0])) & (times < (.800 + toi[1])))[0]
+    gat.estimators_ = [gat.estimators_[t] for t in toi_]
+    gat.train_times_['times'] = [gat.train_times_['times'][t] for t in toi_]
+    gat.train_times_['slices'] = [gat.train_times_['slices'][t] for t in toi_]
+    # predict all trials, including absent to keep cv scheme
+    gat.predict(epochs[sel_gat])
+
+    # subscore on present only
+    gat.scores_ = subscore(gat, sel, y[sel])
+    scores.append(gat.scores_)
+
+    # Save classifier results
+    pkl_fname = paths('decod', subject=subject,
+                      analysis=analysis['name'] + '_probe_to_target')
+    with open(pkl_fname, 'wb') as f:
+        pickle.dump([gat, analysis, sel_gat, events], f)
+
+# XXX FIXME TODO: probe starts at 816, not 800!
+times = epochs.times[:-5]
+score_diag = np.array([np.diag(np.array(score)[4:, :]) for score in scores])
+p_val = stats(score_diag)
+pretty_decod(score_diag, times=times, sig=p_val < .05, color=analysis['color'],
+             fill=True)
+# toi_ = np.where((times >= toi[0]) & (times < toi[1]))[0]
+toi_ = np.where(p_val < .05)[0]
+score = np.mean(score_diag[:, toi_], axis=1)
+print(np.mean(score, axis=0),
+      np.std(score, axis=0) / np.sqrt(len(score_diag)),
+      np.min(p_val[toi_]),
+      times[toi_])
