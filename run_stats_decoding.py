225be3ec1614973c37d5737c5b2823f94754c1f5
kingjr <jeanremi.king+github@gmail.com>
2016-06-01 19:20:40 -0400

    refactor run_stats_decoding

--- run_stats_decoding.py
+++ run_stats_decoding.py
@@ -1,48 +1,40 @@
 """Performs stats across subjects of decoding scores fitted within subjects"""
-import pickle
 import numpy as np
 from base import stats
-from config import paths, subjects
+from config import subjects, load, save
 from conditions import analyses
 
 # For each analysis of interest
 for analysis in analyses:
-    print analysis['name']
     # Load decoding results
+    print('load', analysis['name'])
     scores = list()
-    y_pred = list()
     for subject in subjects:
-        print subject
         # define path to file to be loaded
-        score_fname = paths('score', subject=subject,
+        score, times = load('score', subject=subject,
                             analysis=analysis['name'])
-        with open(score_fname, 'rb') as f:
-            out = pickle.load(f)
-            gat = out[0]
-        scores.append(gat.scores_)
-        y_pred.append(gat.y_pred_)
+        scores.append(score)
 
-    scores = [score for score in scores if not np.isnan(score[0][0])]
+    scores = [sc for sc in scores if not np.isnan(sc[0][0])]
     if len(scores) < 7:
         print('%s: not enough subjects' % analysis['name'])
         continue
     chance = analysis['chance']
     alpha = 0.05
-    times = gat.train_times_['times'] * 1000
 
     # Compute stats: is decoding different from theoretical chance level (using
     # permutations across subjects)
+    print('stats', analysis['name'])
     p_values = stats(np.array(scores) - chance)
-    diag_offdiag = scores - np.tile([np.diag(score) for score in scores],
+    diag_offdiag = scores - np.tile([np.diag(sc) for sc in scores],
                                     [len(times), 1, 1]).transpose(1, 0, 2)
     p_values_off = stats(diag_offdiag)
 
-    scores_diag = [np.diag(score) for score in scores]
+    scores_diag = [np.diag(sc) for sc in scores]
     p_values_diag = stats(np.array(scores_diag)[:, :, None] - chance)
 
     # Save stats results
-    stats_fname = paths('score',  analysis=('stats_' + analysis['name']))
-    with open(stats_fname, 'wb') as f:
-        out = dict(scores=scores, p_values=p_values, p_values_off=p_values_off,
-                   times=times, analysis=analysis, p_values_diag=p_values_diag)
-        pickle.dump(out, f)
+    print('save', analysis['name'])
+    out = dict(scores=scores, p_values=p_values, p_values_off=p_values_off,
+               times=times, analysis=analysis, p_values_diag=p_values_diag)
+    save(out, 'score',  analysis=('stats_' + analysis['name']))
