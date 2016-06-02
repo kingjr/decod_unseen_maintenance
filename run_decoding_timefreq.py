0ec9dfe4712b9fe120d039f978ad103c16c552ed
kingjr <jeanremi.king+github@gmail.com>
2016-06-01 20:00:25 -0400

    fix decoding time_freq

--- run_decoding_timefreq.py
+++ run_decoding_timefreq.py
@@ -8,41 +8,60 @@ from mne.decoding import TimeDecoding
 from config import subjects, load, save
 from conditions import analyses
 
+
+def _run(epochs, events, analysis):
+
+    # Set time frequency parameters
+    start = np.where(epochs.times >= -.200)[0][0]
+    stop = np.where(epochs.times >= 1.400)[0][0]
+    frequencies = np.arange(8, 70, 1.)
+    decim = slice(start, stop, 8)  # ~62 Hz after TFR
+
+    # Get relevant trials
+    query, condition = analysis['query'], analysis['condition']
+    sel = range(len(events)) if query is None \
+        else events.query(query).index
+    sel = [ii for ii in sel if ~np.isnan(events[condition][sel][ii])]
+    y = np.array(events[condition], dtype=np.float32)
+
+    print analysis['name'], np.unique(y[sel]), len(sel)
+
+    if len(sel) == 0:
+        return
+
+    # Apply analysis
+    td = TimeDecoding(clf=analysis['clf'], cv=analysis['cv'],
+                      scorer=analysis['scorer'], n_jobs=-1)
+    tfd = TimeFrequencyDecoding(
+        frequencies, td=td, n_jobs=-1,
+        tfr_kwargs=dict(n_cycles=5, decim=decim))
+    print(subject, analysis['name'], 'fit')
+    tfd.fit(epochs[sel], y=y[sel])
+    print(subject, analysis['name'], 'score')
+    score = tfd.score()
+
+    # save space
+    tfd.td.estimators_ = None
+    if analysis['name'] not in ['target_present', 'target_circAngle']:
+        tfd.td.y_pred_ = None
+
+    # Save analysis
+    print(subject, analysis['name'], 'save')
+    save([tfd, analysis, sel, events], 'decod_tfr',
+         subject=subject, analysis=analysis['name'],
+         upload=True, overwrite=True)
+    save([score, epochs.times[decim], frequencies], 'score_tfr',
+         subject=subject, analysis=analysis['name'],
+         upload=True, overwrite=True)
+
 for s, subject in enumerate(subjects):  # Loop across each subject
     print(subject)
 
     epochs = load('epochs', subject=subject, preload=True)
-    epochs.decimate(4)  # 250 Hz is enough
+    epochs.decimate(2)  # 500 Hz is enough
+    epochs.pick_types(meg=True, eeg=False, stim=False, eog=False, ecg=False)
     events = load('behavior', subject=subject)
-    start = np.where(epochs.times >= -.200)[0][0]
-    stop = np.where(epochs.times >= 1.400)[0][0]
-    frequencies = np.arange(8, 70, 1.)
-    decim = slice(start, stop, 4)  # 61 Hz after TFR
-    times = epochs.times[decim]
 
     # Apply to each analysis
     for analysis in analyses:
-        query, condition = analysis['query'], analysis['condition']
-        sel = range(len(events)) if query is None \
-            else events.query(query).index
-        sel = [ii for ii in sel if ~np.isnan(events[condition][sel][ii])]
-        y = np.array(events[condition], dtype=np.float32)
-
-        print analysis['name'], np.unique(y[sel]), len(sel)
-
-        if len(sel) == 0:
-            continue
-
-        # Apply analysis
-        td = TimeDecoding(clf=analysis['clf'], cv=analysis['cv'],
-                          scorer=analysis['scorer'], n_jobs=-1)
-        tfd = TimeFrequencyDecoding(
-            frequencies, td=td, n_jobs=-1,
-            tfr_kwargs=dict(n_cycles=5, decim=decim))
-        tfd.fit(epochs[sel], y=y[sel])
-        score = tfd.score()
-
-        # Save analysis
-        save([score, frequencies, times], 'score_tfr',
-             subject=subject, analysis=analysis['name'],
-             upload=True, overwrite=True)
+        _run(epochs, events, analysis)
