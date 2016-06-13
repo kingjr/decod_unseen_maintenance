156705aa7c3ea21463d7ac4d63167db9267c04e7
kingjr <jeanremi.king+github@gmail.com>
2016-06-13 12:51:22 -0400

    WIP source cluster

--- plot_source_analysis.py
+++ plot_source_analysis.py
@@ -5,45 +5,38 @@ from mne import morph_data_precomputed
 from matplotlib.colors import LinearSegmentedColormap
 from conditions import analyses
 from jr.plot import alpha_cmap
-from config import load, bad_mri, subjects_id, report, paths, tois
+from config import load, subjects_id, report, tois
 report._setup_provenance()
 
+# get stc for plotting
+stc, _, _ = load('evoked_source', subject=1, analysis='target_present')
+morph = load('morph', subject=1)
+vertices_to = [np.arange(10242)] * 2
+stc_morph = morph_data_precomputed(subjects_id[0], 'fsaverage', stc,
+                                   vertices_to, morph)
 
-morphs = dict()
 for analysis in analyses:
-    stcs = list()
-    stcs2 = list()
-    for meg_subject, subject in zip(range(1, 21), subjects_id):
-        if subject in bad_mri:
-            continue
-        # load
-        stc, _, _ = load('evoked_source', subject=meg_subject,
-                         analysis=analysis['name'])
-        morphs[subject] = load('morph', subject=meg_subject)
-        vertices_to = [np.arange(10242)] * 2
-        # fix angle error scale
-        if 'circAngle' in analysis['name']:
-            stc._data /= 2.
+    stcs, connectivity = load('score_source', analysis=analysis['name'])
+    p_val = load('score_pval', analysis=analysis['name'])
 
-        # apply morph
-        stc_morph = morph_data_precomputed(subject, 'fsaverage', stc,
-                                           vertices_to, morphs[subject])
-        stcs.append(stc_morph.data)
-        # same with absolute values
-        stc._data = np.abs(stc._data - analysis['chance'])
-        stc_morph = morph_data_precomputed(subject, 'fsaverage', stc,
-                                           vertices_to, morphs[subject])
+    # morph pval in log space
+    stc_morph._data[:, :] = np.log(p_val.T)
+    stc_pval = stc_morph.morph('fsaverage', grade=None)
+    sig = np.exp(stc_pval._data) < .05
+    del stc_pval, p_val
 
-        stcs2.append(stc_morph.data + analysis['chance'])
+    # get absolute score value for plotting
+    chance = analysis['chance']
+    stc2 = np.mean(np.abs(stcs - chance) + chance, axis=0)
+    stc = np.mean(stcs, axis=0)
+    del stcs, connectivity
 
-    # plot divergent and absolute score
-    stcs = np.array(stcs)
-    stcs2 = np.array(stcs2)
+    # plot
     cmaps = (alpha_cmap('RdBu_r'),
              alpha_cmap(LinearSegmentedColormap.from_list(
-                        'RdBu', ['w', analysis['color'], 'k']), diverge=False))
-    for data, suf, cmap in zip([stcs, stcs2], ['', '_abs'], cmaps):
-        stc_morph._data = np.mean(data, axis=0)
+                        'RdBu', ['k', analysis['color']]), diverge=False))
+    for data, suf, cmap in zip([stc, stc2], ['', '_abs'], cmaps):
+        stc_morph._data = data
         brain = stc_morph.plot(
             subject='fsaverage',
             hemi='split', views=['lat', 'med'], colormap=cmap,
@@ -52,10 +45,11 @@ for analysis in analyses:
         # center colormap depending on whether abs value or divergent
         if suf == '':
             mM = np.percentile(abs(stc_morph.data), 99.99)
-            clim = (2 * analysis['chance'] - mM, analysis['chance'], mM, False)
+            clim = (2 * chance - mM, chance, mM, False)
         else:
             low, mid, high = np.percentile(abs(stc_morph.data),
                                            [10, 50, 99.99])
+            high = np.max(stc_morph.data)
             clim = (low, mid, high, False)
         brain.scale_data_colormap(*clim)
 
@@ -66,8 +60,23 @@ for analysis in analyses:
         image_path = op.join(report.report.data_path, analysis['name'] + suf)
         if not op.exists(image_path):
             os.makedirs(image_path)
+        contours = list()
         for t in range(0, 1200, 10):
+            for ii in contours:
+                ii['surface'].remove()
             brain.set_time(t)
+            # plot significant clusters
+            overlay = 1. * sig[:, np.where(brain._times >= t)[0][0]]
+            left = brain.add_contour_overlay(
+                overlay[len(overlay)//2:], min=0., max=1.,
+                hemi='lh',
+                n_contours=2, colorbar=False, colormap='hot')
+            contours = brain.contour_list
+            right = brain.add_contour_overlay(
+                overlay[:len(overlay)//2], min=0., max=1.,
+                hemi='rh',
+                n_contours=2, colorbar=False, colormap='hot')
+            contours += brain.contour_list
             fname = '%s_source_%3i%s.png' % (analysis['name'], t, suf)
             brain.save_image(op.join(image_path, fname))
 
@@ -86,18 +95,5 @@ for analysis in analyses:
             fname = '%s_%s_%3i.png' % (analysis['name'], suf, toi[0] * 1e3)
             brain.save_image(op.join(report.report.data_path, fname))
         brain.close()
-
-# Plot individual subject to inspect source reconstruction
-for meg_subject, subject in zip(range(1, 21), subjects_id):
-    if subject in bad_mri:
-        continue
-    stc, _, _ = load('evoked_source', subject=meg_subject,
-                     analysis='target_present')
-    brain = stc.plot(hemi='split', subject=subject, views=['lat', 'med'],
-                     subjects_dir=paths('freesurfer'))
-    brain.set_time(180)
-    image_path = op.join(report.report.data_path)
-    brain.save_image(op.join(image_path, subject + '.png'))
-    brain.close()
 
 # report.save()
