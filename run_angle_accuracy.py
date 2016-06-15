38d3efeb7f97e92167e0acb4b0fd026b533eaef7
kingjr <jeanremi.king+github@gmail.com>
2016-06-15 09:07:54 -0400

    clean plot accuracy

--- run_angle_accuracy.py
+++ run_angle_accuracy.py
@@ -1,40 +1,44 @@
+import os.path as op
 import matplotlib.pyplot as plt
 import numpy as np
 from jr.plot import pretty_decod
 from jr.gat import get_diagonal_ypred, scorer_angle
-from config import subjects, load, report
+from config import subjects, load, report, save, paths
 from base import stats
 
+if not op.exists(paths('score', analysis='accuracy')):
+    scores = np.ones((20, 151, 2))
+    all_y = list()
+    for s, subject in enumerate(subjects):
+        # Load data
+        print s
+        gat, _, events_sel, events = load('decod', subject=subject,
+                                          analysis='target_circAngle')
+        times = gat.train_times_['times']
+        y_true = gat.y_train_
+        y_correct = np.array(events['discrim_correct'])[events_sel]
 
-scores = np.ones((20, 151, 2))
-all_y = list()
-for s, subject in enumerate(subjects):
-    # Load data
-    print s
-    gat, _, events_sel, events = load('decod', subject=subject,
-                                      analysis='target_circAngle')
-    times = gat.train_times_['times']
-    y_true = gat.y_train_
-    y_correct = np.array(events['discrim_correct'])[events_sel]
+        # get diagonal
+        y_pred = np.transpose(get_diagonal_ypred(gat), [1, 0, 2])[..., 0]
+        for correct in range(2):
+            sel = np.where(y_correct == correct)[0]
+            scores[s, :, correct] = scorer_angle(y_true[sel], y_pred[sel, :])
+    save([scores, times], 'score', analysis='accuracy',
+         upload=True, overwrite=True)
 
-    # get diagonal
-    y_pred = np.transpose(get_diagonal_ypred(gat), [1, 0, 2])[..., 0]
-    for correct in range(2):
-        sel = np.where(y_correct == correct)[0]
-        scores[s, :, correct] = scorer_angle(y_true[sel], y_pred[sel, :])
-
+scores, times = load('score', analysis='accuracy')
 # do stats
-pval_incorrect = stats(scores[:, :, 0])
-pval_correct = stats(scores[:, :, 1])
+# pval_incorrect = stats(scores[:, :, 0])
+# pval_correct = stats(scores[:, :, 1])
 pval_diff = stats(scores[:, :, 1] - scores[:, :, 0])
 
 # Plot
-fig, ax = plt.subplots(1)
+fig, ax = plt.subplots(1, figsize=[6.5, 3])
 pretty_decod(scores[:, :, 1] / 2., times=times, sig=pval_diff < .05,
              chance=0., color='y', fill=True, ax=ax)
-pretty_decod(scores[:, :, 1] / 2., times=times, sig=pval_correct < .05,
+pretty_decod(scores[:, :, 1] / 2., times=times,  # sig=pval_correct < .05,
              chance=0., color='r', fill=False, ax=ax)
-pretty_decod(scores[:, :, 0.] / 2., times=times, sig=pval_incorrect < .05,
+pretty_decod(scores[:, :, 0.] / 2., times=times,  # sig=pval_incorrect < .05,
              chance=0., color='b', fill=False, ax=ax)
 
 xlim, ylim = ax.get_xlim(), np.array(ax.get_ylim())
