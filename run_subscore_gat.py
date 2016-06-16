b59d7722d102ad413324cf379783c256d3bbe697
kingjr <jeanremi.king+github@gmail.com>
2016-06-16 13:26:04 -0400

    ENH: slice and toi duration for each visibility score

--- run_subscore_gat.py
+++ run_subscore_gat.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from jr.gat import subscore
 from jr.stats import repeated_spearman
-from jr.plot import pretty_plot, pretty_gat, share_clim, pretty_axes
-from config import subjects, load, save, paths, report
+from jr.plot import (pretty_plot, pretty_gat, share_clim, pretty_axes,
+                     pretty_decod, plot_sem)
+from jr.utils import align_on_diag
+from config import subjects, load, save, paths, report, tois
 from base import stats
 from conditions import analyses
 analyses = [analysis for analysis in analyses if analysis['name'] in
@@ -97,41 +99,131 @@ def _correlate(analysis):
     return all_R, pval, times
 
 
+def _duration_toi(analysis):
+    """Estimate temporal generalization
+    Re-align on diagonal, average per toi and compute stats."""
+    ana_name = analysis['name'] + '-duration-toi'
+    if os.path.exists(paths('score', analysis=ana_name)):
+        return load('score', analysis=ana_name)
+    all_scores, _, times = load('score', analysis=analysis['name'] + '-vis')
+    # Add average duration
+    n_subject = len(all_scores)
+    all_score_tois = np.zeros((n_subject, 4, len(tois), len(times)))
+    all_pval_tois = np.zeros((4, len(tois), len(times)))
+    for vis in range(4):
+        scores = all_scores[:, vis, ...]
+        # align score on training time
+        scores = [align_on_diag(score) for score in scores]
+        # center effect
+        scores = np.roll(scores, len(times) // 2, axis=2)
+        for t, toi in enumerate(tois):
+            toi = np.where((times >= toi[0]) & (times <= toi[1]))[0]
+            score_toi = np.mean(scores[:, toi, :], axis=1)
+            all_score_tois[:, vis, t, :] = score_toi
+            all_pval_tois[vis, t, :] = stats(score_toi - analysis['chance'])
+    save([all_score_tois, all_pval_tois, times], 'score', analysis=ana_name)
+    return [all_score_tois, all_pval_tois, times]
+
+
+# Main plotting
 cmap = plt.get_cmap('bwr')
 colors = cmap(np.linspace(0, 1, 4.))
 for analysis in analyses:
     all_scores, score_pvals, times = _subscore(analysis)
+    if 'circAngle' in analysis['name']:
+        all_scores /= 2.
     # plot subscore GAT
-    fig, axes = plt.subplots(1, 4)
-    for vis, ax in enumerate(axes):
+    figs, axes = list(), list()
+    for vis in range(4):
+        fig, ax = plt.subplots(1, figsize=[7, 5.5])
         scores = all_scores[:, vis, ...]
         p_val = score_pvals[vis]
-        if 'circAngle' in analysis['name']:
-            scores /= 2.
         pretty_gat(np.nanmean(scores, axis=0), times=times,
                    chance=analysis['chance'],
                    sig=p_val < .05, ax=ax, colorbar=False)
         ax.axvline(.800, color='k')
         ax.axhline(.800, color='k')
+        axes.append(ax)
+        figs.append(fig)
     share_clim(axes)
-    ticks = np.arange(-.100, 1.101, .100)
-    ticklabels = [int(1e3 * ii) if ii in [0, .800] else '' for ii in ticks]
-    pretty_axes(axes, xlabel='Test Times', ylabel='Train Times',
-                xticks=ticks, yticks=ticks,
-                xticklabels=ticklabels, yticklabels=ticklabels)
-    report.add_figs_to_section([fig], [analysis['name']], 'subscore')
+    fig_names = [analysis['name'] + str(vis) for vis in range(4)]
+    report.add_figs_to_section(figs, fig_names, 'subscore')
 
-    # plot correlation with visibility
+    # plot GAT slices
+    slices = np.arange(.100, .901, .200)
+    fig, axes = plt.subplots(len(slices), 1, figsize=[5, 6],
+                             sharex=True, sharey=True)
+    for this_slice, ax in zip(slices, axes[::-1]):
+        toi = np.where(times >= this_slice)[0][0]
+        for vis in range(4)[::-1]:
+            if vis not in [0, 3]:
+                continue
+            score = all_scores[:, vis, toi, :]
+            sig = np.array(score_pvals)[vis, toi, :] < .05
+            pretty_decod(score, times, color=colors[vis], ax=ax, sig=sig,
+                         fill=True, chance=analysis['chance'])
+        if ax != axes[-1]:
+            ax.set_xlabel('')
+        ax.axvline(.800, color='k')
+        ax.axvline(this_slice, color='b')
+    lim = np.nanmax(all_scores.mean(0))
+    ticks = np.array([2 * analysis['chance'] - lim, analysis['chance'], lim])
+    ticks = np.round(ticks * 100) / 100.
+    ax.set_ylim(ticks[0], ticks[-1])
+    ax.set_yticks(ticks)
+    ax.set_yticklabels([ticks[0], 'chance', ticks[-1]])
+    ax.set_xlim(-.100, 1.201)
+    for ax in axes:
+        ax.axvline(.800, color='k')
+        if analysis['typ'] == 'regress':
+            ax.set_ylabel('R', labelpad=-15)
+        elif analysis['typ'] == 'categorize':
+            ax.set_ylabel('AUC', labelpad=-15)
+        else:
+            ax.set_ylabel('rad.', labelpad=-15)
+        ax.set_yticklabels(['', '', '%.2f' % ax.get_yticks()[2]])
+    ax.set_xlabel('Times', labelpad=-10)
+    report.add_figs_to_section([fig], [analysis['name']], 'slice_duration')
+
+    # plot average slices toi to show duration
+    all_durations, toi_pvals, times = _duration_toi(analysis)
+    roll_times = times-times[len(times)//2]
+    if 'circAngle' in analysis['name']:
+        all_durations /= 2.
+    fig, axes = plt.subplots(2, 1, sharex=True, sharey=True, figsize=[3, 6])
+    for t, (toi, ax) in enumerate(zip(tois[1:-1], axes[::-1])):
+        for vis in range(4)[::-1]:
+            score = all_durations[:, vis, t+1, :]
+            sig = toi_pvals[vis, t+1, :] < .05
+            plot_sem(roll_times, score, color=colors[vis], alpha=.05, ax=ax)
+            pretty_decod(np.nanmean(score, 0), roll_times,
+                         color=colors[vis],
+                         chance=analysis['chance'], sig=sig, ax=ax)
+        if ax != axes[-1]:
+            ax.set_xlabel('')
+    mean_score = np.nanmean(all_durations[1:-1], axis=0)
+    ticks = np.array([mean_score.min(), analysis['chance'], mean_score.max()])
+    ticks = np.round(ticks * 100) / 100.
+    ax.set_ylim(ticks[0], ticks[-1])
+    ax.set_yticks(ticks)
+    ax.set_yticklabels([ticks[0], 'chance', ticks[-1]])
+    ax.set_xlim(-.700, .700)
+    pretty_plot(ax)
+    report.add_figs_to_section([fig], [analysis['name']], 'toi_duration')
+
+    # plot sig scores and correlation with visibility
     _, R_pval, _ = _correlate(analysis)
-    fig, ax = plt.subplots(1, figsize=[4, 4])
+    fig, ax = plt.subplots(1, figsize=[5, 6])
     for vis in range(4)[::-1]:
+        if vis not in [0, 3]:  # for clarity only plot min max visibility
+            continue
         pval = score_pvals[vis]
         sig = pval > .05
         xx, yy = np.meshgrid(times, times, copy=False, indexing='xy')
         ax.contourf(xx, yy, sig, levels=[-1, 0], colors=[colors[vis]],
-                    aspect='image')
-    # ax.contour(xx, yy, R_pval > .05, levels=[-1, 0], colors='k',
-    #            aspect='image')
+                    aspect='equal')
+    ax.contour(xx, yy, R_pval > .05, levels=[-1, 0], colors='k',
+               aspect='equal')
     ax.axvline(.800, color='k')
     ax.axhline(.800, color='k')
     ticks = np.arange(-.100, 1.101, .100)
@@ -145,6 +237,8 @@ for analysis in analyses:
     ax.set_xlim(-.100, 1.100)
     ax.set_ylim(-.100, 1.100)
     pretty_plot(ax)
+    ax.set_aspect('equal')
     report.add_figs_to_section([fig], [analysis['name']], 'R')
+
 
 report.save()
