88fff44c1125b410f71c5b40004e8422a6aa27aa
kingjr <jeanremi.king+github@gmail.com>
2016-06-17 18:15:58 -0400

    ENH: clean subscore regression

--- run_subscore_gat.py
+++ run_subscore_gat.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from jr.gat import subscore
 from jr.stats import repeated_spearman
 from jr.plot import (pretty_plot, pretty_gat, share_clim, pretty_axes,
-                     pretty_decod, plot_sem)
+                     pretty_decod, plot_sem, bar_sem)
 from jr.utils import align_on_diag
 from config import subjects, load, save, paths, report, tois
 from base import stats
@@ -55,6 +55,99 @@ def _subscore(analysis):
     save([all_scores, pval, times],
          'score', analysis=ana_name, overwrite=True, upload=True)
     return all_scores, pval, times
+
+
+def _average_ypred_toi(gat, toi, analysis):
+    """Average single trial predictions of each time point in a given TOI"""
+    from jr.gat import get_diagonal_ypred
+    y_pred = np.transpose(get_diagonal_ypred(gat), [1, 0, 2])
+    times = gat.train_times_['times']
+    # select time sample
+    toi = np.where((times >= toi[0]) & (times <= toi[1]))[0]
+    if 'circAngle' in analysis['name']:
+        # weight average by regressor radius
+        cos = np.cos(y_pred[:, toi, 0])
+        sin = np.sin(y_pred[:, toi, 0])
+        radius = y_pred[:, toi, 1]
+        y_pred = np.angle(np.median((cos + 1j * sin) * radius, axis=1))
+    else:
+        y_pred = np.median(y_pred[:, toi], axis=1)
+    return y_pred
+
+
+def _subscore_toi_vis(y_pred, events, analysis):
+    """Subscore each visibility"""
+    scores = np.nan * np.zeros(4)
+    for vis in range(4):
+        # select trials according to visibility
+        sel = np.where(events['detect_button'] == vis)[0]
+
+        # for clarity, add all absent trials in target_present analysis
+        if analysis['name'] == 'target_present':
+            sel = np.r_[sel,
+                        np.where(events['target_present'] == False)[0]]  # noqa
+
+        # skip if not enough trials
+        if len(sel) < 5:
+            continue
+        scores[vis] = analysis['scorer'](
+            y_true=events[analysis['name']][sel],
+            y_pred=y_pred[sel])
+    # Do the prediction vary across visibility?
+    sel = np.where(events['detect_button'] >= 0.)[0]
+    R = repeated_spearman(y_pred[sel], events['detect_button'][sel])
+    return scores, R
+
+
+def _subscore_toi_contrast(y_pred, events, analysis):
+    """Subscore each contrast"""
+    scores = np.nan * np.zeros(3)
+    for ii, contrast in enumerate([.5, .75, 1.]):
+        # select trials according to visibility
+        sel = np.where(events['target_contrast'] == contrast)[0]
+        # for clarity, add all absent trials in target_present analysis
+        if analysis['name'] == 'target_present':
+            sel = np.r_[sel,
+                        np.where(events['target_present'] == False)[0]]  # noqa
+
+        scores[ii] = analysis['scorer'](
+            y_true=events[analysis['name']][sel],
+            y_pred=y_pred[sel])
+    # Do the prediction vary across visibility?
+    sel = np.where(events['target_present'])[0]
+    R = repeated_spearman(y_pred[sel], events['target_contrast'][sel])
+    return scores, R
+
+
+def _subscore_toi(analysis):
+    """Subscore each analysis as a function of the reported visibility"""
+    ana_name = analysis['name'] + '-toi'
+
+    # don't recompute if not necessary
+    fname = paths('score', analysis=ana_name)
+    if os.path.exists(fname):
+        return load('score', analysis=ana_name)
+
+    # gather data
+    scores_vis = np.zeros((20, len(tois), 4))
+    R_vis = np.zeros((20, len(tois)))
+    scores_cntrst = np.zeros((20, len(tois), 3))
+    R_cntrst = np.zeros((20, len(tois)))
+    for s, subject in enumerate(subjects):
+        gat, _, events_sel, events = load('decod', subject=subject,
+                                          analysis=analysis['name'])
+        events = events.iloc[events_sel].reset_index()
+        for t, toi in enumerate(tois):
+            # Average predictions on single trials across time points
+            y_pred = _average_ypred_toi(gat, toi, analysis)
+            scores_vis[s, t, :], R_vis[s, t] = _subscore_toi_vis(
+                y_pred, events, analysis)
+            scores_cntrst[s, t, :], R_cntrst[s, t] = _subscore_toi_contrast(
+                y_pred, events, analysis)
+
+    save([scores_vis, R_vis, scores_cntrst, R_cntrst], 'score',
+         analysis=ana_name, overwrite=True, upload=True)
+    return [scores_vis, R_vis, scores_cntrst, R_cntrst]
 
 
 def _correlate(analysis):
@@ -127,7 +220,9 @@ def _duration_toi(analysis):
 
 # Main plotting
 cmap = plt.get_cmap('bwr')
-colors = cmap(np.linspace(0, 1, 4.))
+colors_vis = cmap(np.linspace(0, 1, 4.))
+cmap = plt.get_cmap('hot_r')
+colors_contrast = cmap([.5, .75, 1.])
 for analysis in analyses:
     all_scores, score_pvals, times = _subscore(analysis)
     if 'circAngle' in analysis['name']:
@@ -160,7 +255,7 @@ for analysis in analyses:
                 continue
             score = all_scores[:, vis, toi, :]
             sig = np.array(score_pvals)[vis, toi, :] < .05
-            pretty_decod(score, times, color=colors[vis], ax=ax, sig=sig,
+            pretty_decod(score, times, color=colors_vis[vis], ax=ax, sig=sig,
                          fill=True, chance=analysis['chance'])
         if ax != axes[-1]:
             ax.set_xlabel('')
@@ -195,9 +290,10 @@ for analysis in analyses:
         for vis in range(4)[::-1]:
             score = all_durations[:, vis, t+1, :]
             sig = toi_pvals[vis, t+1, :] < .05
-            plot_sem(roll_times, score, color=colors[vis], alpha=.05, ax=ax)
+            plot_sem(roll_times, score, color=colors_vis[vis], alpha=.05,
+                     ax=ax)
             pretty_decod(np.nanmean(score, 0), roll_times,
-                         color=colors[vis],
+                         color=colors_vis[vis],
                          chance=analysis['chance'], sig=sig, ax=ax)
         if ax != axes[-1]:
             ax.set_xlabel('')
@@ -220,7 +316,7 @@ for analysis in analyses:
         pval = score_pvals[vis]
         sig = pval > .05
         xx, yy = np.meshgrid(times, times, copy=False, indexing='xy')
-        ax.contourf(xx, yy, sig, levels=[-1, 0], colors=[colors[vis]],
+        ax.contourf(xx, yy, sig, levels=[-1, 0], colors=[colors_vis[vis]],
                     aspect='equal')
     ax.contour(xx, yy, R_pval > .05, levels=[-1, 0], colors='k',
                aspect='equal')
@@ -240,5 +336,78 @@ for analysis in analyses:
     ax.set_aspect('equal')
     report.add_figs_to_section([fig], [analysis['name']], 'R')
 
+    # plot and report subscore per visibility and contrast for each toi
+    from scipy.stats import wilcoxon
+    from jr.utils import table2html
+    toi_vis, toi_R_vis, toi_cntrst, toi_R_cntrst = _subscore_toi(analysis)
+
+    # report angle error because orientation
+    if 'circAngle' in analysis['name']:
+        toi_vis /= 2.
+        toi_cntrst /= 2.
+
+    def quick_stats(x, chance):
+        x = x[np.where(~np.isnan(x))[0]]
+        text = '[%.3f+/-%.3f, p=%.4f]'
+        m = np.nanmean(x)
+        sem = np.nanstd(x) / np.sqrt(len(x))
+        pval = wilcoxon(x - chance)[1]
+        return text % (m, sem, pval)
+
+    # subscore visibilty then subscore contrast
+    for name, toi_scores, toi_R, colors in (
+        ('visibility', toi_vis, toi_R_vis, colors_vis),
+            ('contrast', toi_cntrst, toi_R_cntrst, colors_contrast)):
+
+        n_subscore = 3 if name == 'contrast' else 4
+        fig, axes = plt.subplots(1, len(tois), sharey=True, figsize=[6, 2])
+        table = np.empty((len(tois), n_subscore + 3), dtype=object)
+
+        # effect in each toi
+        for toi, ax in enumerate(axes):
+            bar_sem(range(n_subscore),
+                    toi_scores[:, toi, :] - analysis['chance'],
+                    bottom=analysis['chance'], ax=ax, color=colors)
+            for ii in range(n_subscore):
+                score_ = toi_scores[:, toi, ii]
+                table[toi, ii] = quick_stats(toi_scores[:, toi, ii],
+                                             chance=analysis['chance'])
+            # difference min max (e.g. seen - unseen)
+            table[toi, n_subscore] = quick_stats(toi_scores[:, toi, -1] -
+                                                 toi_scores[:, toi, 0], 0.)
+            # regression across scores: single trials
+            table[toi, n_subscore + 1] = quick_stats(toi_R[:, toi], chance=0.)
+
+            # regression across scores: not single trials
+            R = [repeated_spearman(range(n_subscore), subject)[0]
+                 for subject in toi_scores[:, toi, :]]
+            table[toi, n_subscore + 2] = quick_stats(np.array(R), chance=0.)
+
+        pretty_axes(axes, xticks=[])
+        table = np.c_[[str(t) for t in tois], table]
+
+        headers = [name + str(ii) for ii in range(n_subscore)]
+        table = np.vstack((np.r_[[''], headers, ['max-min'],
+                                 ['R', 'R (not single trials)']], table))
+        report.add_htmls_to_section(table2html(table),
+                                    'subscore_' + name, analysis['name'])
+
+        # Does the effect vary over time
+        # e.g. seen-unseen stronger in early vs late
+        table = np.empty((len(tois), len(tois)), dtype=object)
+        for t1 in range(len(tois)):
+            for t2 in range(len(tois)):
+                table[t1, t2] = quick_stats(toi_R[:, t1] - toi_R[:, t2], 0.)
+        report.add_htmls_to_section(table2html(table),
+                                    'toi_toi_' + name, analysis['name'])
+        report.add_figs_to_section([fig], [name], analysis['name'])
+    # Do contrast and visibility affect different time points? FIXME:
+    # Not the right test?
+    table = np.empty(len(tois), dtype=object)
+    for toi in range(len(tois)):
+        table[toi] = quick_stats(toi_R_vis[:, toi] - toi_R_cntrst[:, toi], 0.)
+    table = np.vstack(([str(toi) for toi in tois], table))
+    report.add_htmls_to_section(table2html(table),
+                                'toi_subscore_', analysis['name'])
 
 report.save()
