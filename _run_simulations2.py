5c0afeb4887a32a6ff815ffda4b7e57e77cd5b87
kingjr <jeanremi.king@gmail.com>
2016-09-22 14:54:39 -0400

    ENH: revision models

--- _run_simulations2.py
+++ _run_simulations2.py
@@ -1,16 +1,14 @@
 """
-These simulations aims at showing how different types of underlying sources may
-reproduce the decoding results we observe: i.e.
+These simulations aim at showing how different types of underlying sources may
+reproduce the 3 main decoding results we observed: i.e.
 1) A significant decoding of the target across different visibility levels,
 2) A significant reduction of decoding accuracy after ~ 250 ms,
 3) A significant correlation between decoding accuracy and the subjective,
-   visibility of the stimulus after the stimulus onset,
-4) A significant decoding of subjective visiblity (which actually necessarily
-derives from 3.)
+   visibility of the stimulus after the stimulus onset)
 """
-
+from collections import OrderedDict as dict
 import numpy as np
-from jr.plot import pretty_decod, pretty_plot
+from jr.plot import pretty_plot
 from jr.gat import scorer_auc, scorer_spearman
 from mne.decoding import GeneralizationAcrossTime
 from mne import create_info, EpochsArray
@@ -79,6 +77,7 @@ def simulate_model(sources, mixin, background, snr=.5, n_trial=100):
     epochs = EpochsArray(X, create_info(n_chan, sfreq, 'mag'), tmin=times[0],
                          proj=False, baseline=None)
 
+    # Temporal generalization pipeline
     gat = GeneralizationAcrossTime(clf=analysis['clf'], cv=8,
                                    scorer=scorer_auc, n_jobs=-1,
                                    score_mode='mean-sample-wise')
@@ -106,131 +105,182 @@ def simulate_model(sources, mixin, background, snr=.5, n_trial=100):
     return np.array(score), np.squeeze(score_vis), np.squeeze(corr_vis)
 
 # Architectures
-t0 = 10
-encoding = dict(high=np.zeros((n_source, n_time)),
-                low=np.zeros((n_source, n_time)))
-two_steps = dict(high=np.zeros((n_source, n_time)),
-                 low=np.zeros((n_source, n_time)))
-reactivation = dict(high=np.zeros((n_source, n_time)),
+t0 = 10  # onset of the first evoked response
+init = lambda: dict(high=np.zeros((n_source, n_time)),
                     low=np.zeros((n_source, n_time)))
-multisteps = dict(high=np.zeros((n_source, n_time)),
-                  low=np.zeros((n_source, n_time)))
-silent = dict(high=np.zeros((n_source, n_time)),
-              low=np.zeros((n_source, n_time)))
 
-# 1. visibility could be due to an encoding problem: the higher the response,
-# the longer its maintenance
-encoding['low'][0, t0:(t0+50)] = np.logspace(1.5, 0, 50) / 100. * 3
-encoding['high'][0, t0:(t0+50)] = np.logspace(1, 0, 50) / 10. * 3
 
-# 2. visibility could be due to a late stage maintenance
+# single stage:
+# visibility could be due to an single problem: the late_maintain the
+# response, the longer its maintain
+single = init()
 for vis in ['low', 'high']:
-    two_steps[vis][0, t0:(t0+10)] = 1
-two_steps['low'][1, t0+10:(t0+50)] = .25
-two_steps['high'][1, t0+10:(t0+50)] = .5
+    single[vis][0, t0:(t0+10)] = 1.
+single['low'][0, (t0+10):60] = .25
+single['high'][0, (t0+10):60] = .5
 
-# 3: two steps encoding but reactivation = aware
+# Early maintain:
+early_maintain = init()
+# visibility depends on the amplitude of the late stage
 for vis in ['low', 'high']:
-    reactivation[vis][0, t0:(t0+10)] = 1
-    reactivation[vis][1, t0+10:(t0+20)] = .5
-    reactivation[vis][2, t0+20:(t0+50)] = .25
-reactivation['high'][0, t0+20:(t0+50)] = .25
+    early_maintain[vis][0, t0:(t0+10)] = 1.
+    early_maintain[vis][0, (t0+10):60] = .25
+early_maintain['low'][1, (t0+10):(t0+10)] = 0.
+early_maintain['high'][1, (t0+10):60] = .5
 
-# 4. Purely dynamical
+# Re-entry:
+# visibility depends on the amplitude a single early stage
+reentry = init()
 for vis in ['low', 'high']:
-    multisteps[vis][0, t0:(t0+10)] = 1
+    reentry[vis][0, t0:(t0+10)] = 1.
+    reentry[vis][1, (t0+10):60] = .25
+reentry['low'][0, (t0+10):60] = 0.
+reentry['high'][0, (t0+10):60] = .5
+
+# Late-maintain
+# visibility depends on the amplitude of a single late stage
+late_maintain = init()
+for vis in ['low', 'high']:
+    late_maintain[vis][0, t0:(t0+10)] = 1.
+late_maintain['low'][1, (t0+10):(t0+50)] = .25
+late_maintain['high'][1, (t0+10):(t0+50)] = .5
+
+# Dynamic
+# visibility depends on the amplitude of the many late stages
+dynamic_amplitude = init()
+for vis in ['low', 'high']:
+    dynamic_amplitude[vis][0, t0:(t0+10)] = 1.
+for stage in range(1, 5):
+    start = t0 + 10 * stage
+    dynamic_amplitude['low'][stage, start:(start+10)] = .25
+    dynamic_amplitude['high'][stage, start:(start+10)] = .5
+
+# Dynamic maintenance
+# visibility depends on the duration of the many late stages
+dynamic_maintain = init()
+for vis in ['low', 'high']:
+    dynamic_maintain[vis][0, t0:(t0+10)] = 1.
+for stage in range(1, 5):
+    start = t0 + 10 * stage
+    # low visibility = transient response
+    dynamic_maintain['low'][stage, start:(start + 10)] = .42
+    # high visibility = sustained response
+    dynamic_maintain['high'][stage, start:60] = .42
+
+# Hybrid model illustrate MEG results
+hybrid = init()
+for vis in ['low', 'high']:
+    hybrid[vis][0, t0:(t0+5)] = 1.
+    # Partial early reversal independent of visibility
+    hybrid[vis][0, (t0+5):(t0+10)] = -.5
+    # Reactivation independent of visibility
+    hybrid[vis][0, (t0+15):(t0+25)] = .25
 for ii in range(1, 5):
-    multisteps['low'][ii, (t0 + 10*ii):(t0+((ii + 1)*10))] = .25
-    multisteps['high'][ii, (t0 + 10*ii):(t0+((ii + 1)*10))] = .5
+    start = (t0 - 4 + 9*ii)
+    # meta stable in low visibility
+    hybrid['low'][ii, start:(start + 20)] = .25
+    # longer metastability and stronger amplitude in high visibility
+    hybrid['high'][ii, start:(start + 30)] = .42
+# Remove signal after 60 to get similar decoding
+for vis in ['low', 'high']:
+    hybrid[vis][:, 60:] = 0.
 
-
-# 5. Silent
-silent['low'][0, t0:(t0+10)] = 1
-silent['low'][0, (t0+40):(t0+50)] = .5
-silent['high'][0, t0:(t0+10)] = 1
-silent['high'][0, (t0+40):(t0+50)] = 1
-
-# empirical scenario: multisteps plus maintenance
-sources = dict(encoding=encoding,
-               two_steps=two_steps,
-               reactivation=reactivation,
-               multisteps=multisteps,
-               silent=silent)
+# empirical scenario: dynamic_amplitude plus maintain
+sources = (
+    ('single', single),
+    ('reentry', reentry),
+    ('late_maintain', late_maintain),
+    ('early_maintain', early_maintain),
+    ('dynamic_amplitude', dynamic_amplitude),
+    ('dynamic_maintain', dynamic_maintain),
+    ('hybrid', hybrid))
 
 n_subject = 20
 all_scores, all_scores_vis, all_corr_vis = dict(), dict(), dict()
-for name, source in sources.iteritems():
+for name, source in sources:
+    # init results
     all_scores[name] = np.zeros((n_subject, 2, n_time, n_time))
     all_scores_vis[name] = np.zeros((n_subject, n_time, n_time))
     all_corr_vis[name] = np.zeros((n_subject, n_time, n_time))
     for subject in range(n_subject):
         # Define a subject specific background activity to mimmic mask, etc
         background = np.random.randn(n_source, n_time) / 10.
+        # Define subject specific foward model
         mixin = np.random.randn(n_source, n_chan)
+        # simulate
         score, score_vis, corr_vis = simulate_model(source, mixin, background)
+        # store
         all_scores[name][subject] = score
         all_scores_vis[name][subject] = score_vis
         all_corr_vis[name][subject] = corr_vis
 
-fig = plt.figure(figsize=[6, 4])
-cmap_contrast = plt.get_cmap('hot_r')
-
-fig_dec, axes_dec = plt.subplots(len(sources), 1, sharex=True, sharey=True)
-fig_gat, axes_gat = plt.subplots(2, len(sources), figsize=[12, 4])
-fig_src, axes_src = plt.subplots(2, len(sources), figsize=[12, 4])
-fig_vis, axes_vis = plt.subplots(1, len(sources), figsize=[12, 2.5])
-fig_vis_corr, axes_corr = plt.subplots(1, len(sources), figsize=[12, 2.5])
+# Plot
+report._setup_provenance()
 for ii, ((model, scores), scores_vis, corr_vis) in enumerate(zip(
         all_scores.iteritems(), all_scores_vis.itervalues(),
         all_corr_vis.itervalues())):
-    for jj, score in enumerate(scores.transpose([1, 0, 2, 3])[[1, 0]]):
-        # Source
-        vis = 'high' if jj == 0 else 'low'
-        linestyle = '--' if vis == 'low' else '-'
-        cmap = plt.get_cmap('rainbow')
-        n_source_ = sum(np.sum(sources[model][vis], axis=1) > 1)
-        colors = cmap(np.linspace(0, 1., n_source_))
-        for src, color in zip(sources[model][vis], colors):
-            pretty_decod(src, ax=axes_src[jj, ii], fill=True, chance=0.,
-                         sig=np.ones_like(src), color=color[:3], alpha=.5)
-        axes_src[jj, ii].set_xlabel('')
-        axes_src[jj, ii].set_ylabel('')
-        axes_src[jj, ii].set_xticks([])
-        axes_src[jj, ii].set_yticks([])
+    fig, axes = plt.subplots(1, 5, figsize=[20, 3])
+    axes = dict(source=axes[0], decod=axes[1], high=axes[2], low=axes[3],
+                vis=axes[4])
+    # order scores by visibility and present seen first
+    scores_ = dict(high=scores[:, 1], low=scores[:, 0])
+    colors = dict(high='r', low='b')
 
-        # Gat
-        axes_gat[jj, ii].matshow(score.mean(0), origin='lower',
-                                 vmin=0, vmax=1., cmap='RdBu_r')
-        axes_gat[jj, ii].set_xticks([])
-        axes_gat[jj, ii].set_yticks([])
-        pretty_plot(axes_gat[jj, ii])
+    # get model
+    source = dict(sources)[model]
+    for vis in ('low', 'high'):
+        score = scores_[vis]
+        # plot sources
+        n_source_ = sum(np.sum(source[vis], axis=1) > 1)
+        y_shift = .2 * float(vis == 'high') - 1.
+        for ii, src in enumerate(source[vis]):
+            if sum(src) == 0:
+                continue
+            y_shift += 1.4
+            zorder = - y_shift + .5 * float(vis == 'low')
+            times = np.linspace(0, 1, len(src)) + .05 * int(vis == 'low')
+            axes['source'].fill_between(
+                times, y_shift, y_shift + src, color=colors[vis],
+                alpha=1., linewidth=0., zorder=zorder)
+            axes['source'].plot(
+                times, y_shift + src, color='k',
+                zorder=zorder)
+
+        axes['source'].set_xticks([])
+        axes['source'].set_yticks([])
+        axes['source'].set_ylim(-1, y_shift + 1.5)
+        for side in ('left', 'right', 'top', 'bottom'):
+            axes['source'].spines[side].set_visible(False)
 
         # Diagonal score
         diag = np.array([np.diag(s) for s in score])
-        # sig = stats(diag - .5) < .05
-        color = 'r' if vis == 'high' else 'b'
-        pretty_decod(diag, chance=.5, fill=True, color=color,
-                     ax=axes_dec[ii], alpha=.1)
-        axes_dec[ii].set_xticks([])
-        axes_dec[ii].set_yticks([])
-        axes_dec[ii].set_xlabel('')
+        zorder = float(vis == 'low')
+        y_shift = .1 * float(vis == 'high')
+        times = np.linspace(0, 2., len(diag.mean(0))) + .1 * int(vis == 'low')
+        axes['decod'].fill_between(
+            times, .5 + y_shift, diag.mean(0) + y_shift, color=colors[vis],
+            alpha=1., linewidth=0., zorder=zorder)
+        axes['decod'].plot(times, diag.mean(0) + y_shift, color='k',
+                           zorder=zorder)
+        axes['decod'].set_xticks([])
+        axes['decod'].set_yticks([])
+        axes['decod'].set_aspect('equal')
+        for side in ('left', 'right', 'top', 'bottom'):
+            axes['decod'].spines[side].set_visible(False)
+        axes['decod'].set_ylim(-.1, 1.1)
 
-    # Gat visibility decoding
-    axes_vis[ii].matshow(scores_vis.mean(0), origin='lower',
-                         vmin=0, vmax=1., cmap='RdBu_r')
-    axes_vis[ii].set_xticks([])
-    axes_vis[ii].set_yticks([])
-    pretty_plot(axes_vis[ii])
+        # Temporal generalization scores
+        axes[vis].matshow(score.mean(0), origin='lower',
+                          vmin=0, vmax=1., cmap='RdBu_r')
+        axes[vis].set_xticks([])
+        axes[vis].set_yticks([])
+        pretty_plot(axes[vis])
 
-    # Gat visibility subtraction
-    axes_corr[ii].matshow(np.mean(scores[:, 1] - scores[:, 0], axis=0),
-                          origin='lower', vmin=-.25, vmax=.25, cmap='RdBu_r')
-    axes_corr[ii].set_xticks([])
-    axes_corr[ii].set_yticks([])
-    pretty_plot(axes_corr[ii])
-
-
-report.add_figs_to_section([fig_gat, fig_dec, fig_src, fig_vis, fig_vis_corr],
-                           ['gat', 'decod', 'source', 'vis', 'corr_vis'],
-                           'simulation')
+    # TG subtraction: high vis - low vis
+    axes['vis'].matshow(np.mean(scores[:, 1] - scores[:, 0], axis=0),
+                        origin='lower', vmin=-.25, vmax=.25, cmap='RdBu_r')
+    axes['vis'].set_xticks([])
+    axes['vis'].set_yticks([])
+    pretty_plot(axes['vis'])
+    report.add_figs_to_section([fig], [model], model)
 report.save()
