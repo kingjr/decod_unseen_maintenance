86dcf2383abfb8cd21cdb24b796bb5fee39a8ba7
kingjr <jeanremi.king@gmail.com>
2016-09-22 14:55:25 -0400

    plot graphs

--- plot_graphs.py
+++ plot_graphs.py
@@ -8,133 +8,104 @@ from conditions import analyses
 from base import stats
 
 analyses = [a for a in analyses if a['name'] in
-            ['target_present', 'target_circAngle', 'detect_button_pst']]
+            ['target_present', 'target_circAngle']]
 
+all_scores = list()
 for analysis in analyses:
     # load data
     out = load('score', analysis='stats_' + analysis['name'])
-    scores = np.array(out['scores']) - analysis['chance']
-    p_values = out['p_values']
+    scores = np.array(out['scores'])
     times = out['times']
+    toi = np.where(times < 1.200)[0]
 
-    # compute stats
-    p_values = stats(scores)
+    # normalize to combine different analyses
+    scores = np.mean(scores, axis=0)
+    scores -= analysis['chance']
+    scores[out['p_values'] > .05] = 0.
+    scores /= np.max(np.diag(scores))
+    scores = scores[toi, :][:, toi]
+    # all_scores.append(scores)
 
-    # normalize
-    weights = np.mean(scores, axis=0)
+    # weights = np.mean(all_scores, axis=0)
+    weights = scores
+
+    # MDS
+    mds = MDS(dissimilarity='precomputed',  random_state=0)
+    abs_weights = (weights + weights.T) / 2.
+    abs_weights = np.abs(abs_weights)
+    pos = mds.fit_transform(abs_weights.max() - abs_weights)
+
+    # plot
     weights /= np.percentile(np.abs(weights), 95)
-    # fix bug
-    weights[0, :] = 0
-    weights[:, 0] = 0
     node_size = np.abs(np.diagonal(weights)) * 100
-    weights *= p_values < .05
-    weights *= 10  # to see the weights
+    weights *= 2  # to see the weights
     clim = [-1., 1.]
     nx_params = dict(edge_curve=False, node_alpha=1., negative_weights=True,
                      edge_alpha=.05, weights_scale=1, clim=clim,
-                     final_pos='horizontal', prune=p_values > .05,
+                     final_pos='horizontal',
                      edge_color=plt.get_cmap('coolwarm'))
 
-    # # snapshots construction ------------------------------------------------
-    # fig, axes = plt.subplots(1, 4, figsize=[12, 3])
-    # fig.subplots_adjust(wspace=0, hspace=0)
-    # for iteration, ax in zip([0, 3, 6, 9], axes):
-    #     G, nodes, edges = plot_graph(weights, ax=ax,
-    #                                  iterations=int(iteration),
-    #                                  node_size=node_size / 3., **nx_params)
-    #     nodes._alpha = 0.
-    #     nodes.set_edgecolors((1., 1., 1., 0.))
-    #     ax.patch.set_visible(False)
-    # report.add_figs_to_section(fig, analysis['name'], 'build')
-    #
-    # # animation construction ------------------------------------------------
-    # images = list()
-    # for iteration in np.logspace(0, .9, 50):
-    #     print iteration
-    #     fig, ax = plt.subplots(1, figsize=[5, 5], facecolor='w')
-    #     G, nodes, edges = plot_graph(weights, ax=ax,
-    #                                  iterations=int(iteration),
-    #                                  node_size=node_size / 3., **nx_params)
-    #     nodes._alpha = 0.
-    #     nodes.set_edgecolors((1., 1., 1., 0.))
-    #     im = Figtodat.fig2img(fig)
-    #     images.append(im)
-    # name = "/network_construction%s.gif" % analysis['name']
-    # writeGif(report.report.data_path + name, images)
-    # report.add_images_to_section(report.report.data_path + name,
-    #                              analysis['name'], 'build')
-    # plt.close('all')
-
-    # MDS
-    mds = MDS(dissimilarity='precomputed')
-    X = np.abs(weights - analysis['chance'])
-    X = (X+X.T) / 2.
-    pos = mds.fit_transform(X.max() - X)
-
-    # final construction ------------------------------------------------------
     fig, ax = plt.subplots(1, figsize=[10, 10])
     G, nodes, edges = plot_graph(weights, ax=ax, iterations=0,
+                                 node_color=plt.cm.plasma,
                                  node_size=node_size, init_pos=pos.T,
                                  **nx_params)
     nodes._alpha = 0.
     nodes.set_edgecolors((1., 1., 1., 0.))
     ax.patch.set_visible(False)
-    plt.show()
-    0/0
     report.add_figs_to_section(fig, analysis['name'], 'network')
 
-    # # Dynamics --------------------------------------------------------------
-    # fig, ax = plt.subplots(1, figsize=[5, 5])
-    # G, nodes, edges = plot_graph(weights, ax=ax, iterations=100,
-    #                              node_size=node_size, **nx_params)
-    # fig.set_facecolor(None)
-    # anim = animate_graph(weights, G, nodes, times=times * 1e3,
-    #                      clim=clim, cmap='RdBu_r')
-    # gif_name = report.report.data_path + '/' + analysis['name'] + '_dyn.gif'
-    # anim.save(gif_name, writer='imagemagick', dpi=75)
-    # anim = animate_graph(weights[::2, :], G, nodes,
-    #                      times=times[::2] * 1e3, clim=clim,
-    #                      cmap='RdBu_r')
-    # report.add_images_to_section(gif_name, analysis['name'], 'gif')
-    # gif_name = gif_name[:-4] + '_fast.gif'
-    # anim.save(gif_name, writer='imagemagick', dpi=50)
-    # report.add_images_to_section(gif_name, analysis['name'] + 'fast', 'gif')
+# Dynamics seen unseen --------------------------------------------------------
+#
+# fig.set_facecolor(None)
+# anim = animate_graph(weights, G, nodes, times=times * 1e3,
+#                      clim=clim, cmap='RdBu_r')
+# gif_name = report.report.data_path + '/' + analysis['name'] + '_dyn.gif'
+# anim.save(gif_name, writer='imagemagick', dpi=75)
+# anim = animate_graph(weights[::2, :], G, nodes,
+#                      times=times[::2] * 1e3, clim=clim,
+#                      cmap='RdBu_r')
+# report.add_images_to_section(gif_name, analysis['name'], 'gif')
+# gif_name = gif_name[:-4] + '_fast.gif'
+# anim.save(gif_name, writer='imagemagick', dpi=50)
 
-    def snapshot(time, title, clim=None):
-        dynamics = np.mean(scores, axis=0)
-        if clim is None:
-            cmax = np.percentile(np.abs(dynamics), 99)
-            clim = -cmax, cmax
-        cmap = plt.get_cmap('bwr')
-        nframe = np.where(times > time)[0][0]
-        dynamic = dynamics[nframe, :]
-        colors = list()
-        zorders = list()
-        sizes = list()
-        for ii in G.nodes():
-            color = (dynamic[ii] - clim[0]) / np.ptp(clim)
-            color = color if color < 1. else 1.
-            color = color if color > 0. else 0.
-            colors.append(cmap(color))
-            zorders.append(dynamic[ii])
-            sizes.append(np.abs(dynamic[ii]) * 500)
-        nodes.set_facecolors(colors)
-        nodes.set_sizes(sizes)
-        zorders = np.argsort(zorders) + 10000
-        # nodes.set_zorder(zorders)
-        nodes.set_edgecolors(colors)
-        ax.set_title('%s ms' % (int(times[nframe] * 100) * 10),
-                     fontdict=dict(horizontalalignment='left'))
-        fig.canvas.draw()
-        fig.tight_layout()
-        fname = report.report.data_path + '/' + analysis['name']
-        fig.savefig(fname + '_dyn%i.png' % (time * 1e3),
-                    dpi=100, transparent=True,)
-        report.add_figs_to_section(fig, analysis['name'] + str((time * 1e3)),
-                                   'snapshot')
+# report.add_images_to_section(gif_name, analysis['name'] + 'fast', 'gif')
 
-    # tois = [150, 190, 310, 410, 610, 930, 990]
-    tois = np.arange(100, 1000, 100)
-    for t in tois:
-        snapshot(t/1e3, '%i ms' % (1e3 * t))
+# def snapshot(time, title, clim=None):
+#     dynamics = np.mean(scores, axis=0)
+#     if clim is None:
+#         cmax = np.percentile(np.abs(dynamics), 99)
+#         clim = -cmax, cmax
+#     cmap = plt.get_cmap('bwr')
+#     nframe = np.where(times > time)[0][0]
+#     dynamic = dynamics[nframe, :]
+#     colors = list()
+#     zorders = list()
+#     sizes = list()
+#     for ii in G.nodes():
+#         color = (dynamic[ii] - clim[0]) / np.ptp(clim)
+#         color = color if color < 1. else 1.
+#         color = color if color > 0. else 0.
+#         colors.append(cmap(color))
+#         zorders.append(dynamic[ii])
+#         sizes.append(np.abs(dynamic[ii]) * 500)
+#     nodes.set_facecolors(colors)
+#     nodes.set_sizes(sizes)
+#     zorders = np.argsort(zorders) + 10000
+#     # nodes.set_zorder(zorders)
+#     nodes.set_edgecolors(colors)
+#     ax.set_title('%s ms' % (int(times[nframe] * 100) * 10),
+#                  fontdict=dict(horizontalalignment='left'))
+#     fig.canvas.draw()
+#     fig.tight_layout()
+#     fname = report.report.data_path + '/' + analysis['name']
+#     fig.savefig(fname + '_dyn%i.png' % (time * 1e3),
+#                 dpi=100, transparent=True,)
+#     report.add_figs_to_section(fig, analysis['name'] + str((time * 1e3)),
+#                                'snapshot')
+#
+# # tois = [150, 190, 310, 410, 610, 930, 990]
+# tois = np.arange(100, 1000, 100)
+# for t in tois:
+#     snapshot(t/1e3, '%i ms' % (1e3 * t))
 report.save()
